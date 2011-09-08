9811c49d59e91db5821af4468ebc56b106b6892e
virushuo <virushuo@gmail.com>
2011-09-08 21:58:44 +0800

    alter db for cookie login

---
+++ db/20110908_add_cookie_autologin.sql
@@ -0,0 +1,2 @@
+ALTER TABLE  `users` ADD  `cookie_logintoken` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
+ALTER TABLE  `users` ADD  `cookie_loginsequ` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ;
