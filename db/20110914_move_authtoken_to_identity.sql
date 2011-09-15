d51451c374e0ddd92539ff4c5707759c0d66f596
virushuo <virushuo@gmail.com>
2011-09-15 15:56:21 +0800

    add token field to db

--- db/20110914_move_authtoken_to_identity.sql
+++ db/20110914_move_authtoken_to_identity.sql
@@ -1,3 +1,3 @@
-ALTER TABLE  `users` DROP  `authentication_token`
+ALTER TABLE  `users` DROP  `authentication_token`;
 ALTER TABLE  `users` ADD  `auth_token` VARCHAR( 32 ) NOT NULL ;
-ALTER TABLE  `users` ADD INDEX (  `auth_token` )
+ALTER TABLE  `users` ADD INDEX (  `auth_token` );
