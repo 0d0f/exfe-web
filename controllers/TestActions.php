ca04ebef59305d35c07d6a3c5e4be47e2b134ab1
virushuo <virushuo@gmail.com>
2011-10-12 14:00:12 +0800

    template email for conversation

--- controllers/TestActions.php
+++ controllers/TestActions.php
@@ -24,44 +24,68 @@ class TestActions extends ActionController {
         $r=$external_identity=mysql_real_escape_string("123");;
         var_dump($r);
     }
+
+    public function getMail($mail)
+    {
+        $title=$mail["title"];
+        $from_name=$mail["from_name"];
+        $action=$mail["action"];
+        $content=$mail["content"];
+        $link=$mail["link"];
+        $command=$mail["command"];
+        $templatename="conversation";
+        $template=file_get_contents("./".$templatename."_template");
+
+        print $template;
+
+
+        print_r($mail);
+
+    }
+
     public function doTest()
     {
-        $invitationdata=$this->getModelByName("invitation");
-        $invitations=$invitationdata->getInvitation_Identities(3);
 
-        $crossData=$this->getModelByName("X");
-        $cross=$crossData->getCross(3);
-        $place_id=$cross["place_id"];
-        if(intval($place_id)>0)
-        {
-            $placeData=$this->getModelByName("place");
-            $place=$placeData->getPlace($place_id);
-            $cross["place"]=$place;
-        }
-        print_r($cross);
-        $invitation=$invitations[0];
-        $args = array(
-                 'title' => $cross["title"],
-                 'description' => $cross["description"],
-                 'begin_at' => $cross["begin_at"],
-                 'place_line1' => $cross["place"]["line1"],
-                 'place_line2' => $cross["place"]["line2"],
-                 'cross_id_base62' => int_to_base62($cross_id),
-                 'invitation_id' => $invitation["invitation_id"],
-                 'token' => $invitation["token"],
-                 'identity_id' => $invitation["identity_id"],
-                 'provider' => $invitation["provider"],
-                 'external_identity' => $invitation["external_identity"],
-                 'name' => $invitation["name"],
-                 'avatar_file_name' => $invitation["avatar_file_name"]
-         );
+        $mail=array();
+        $mail["title"]="mail title";
+        $this->getMail($mail);
 
-        require 'lib/Resque.php';
-        date_default_timezone_set('GMT');
-        Resque::setBackend('127.0.0.1:6379');
-
-
-        print $str;
+#        $invitationdata=$this->getModelByName("invitation");
+#        $invitations=$invitationdata->getInvitation_Identities(3);
+#
+#        $crossData=$this->getModelByName("X");
+#        $cross=$crossData->getCross(3);
+#        $place_id=$cross["place_id"];
+#        if(intval($place_id)>0)
+#        {
+#            $placeData=$this->getModelByName("place");
+#            $place=$placeData->getPlace($place_id);
+#            $cross["place"]=$place;
+#        }
+#        print_r($cross);
+#        $invitation=$invitations[0];
+#        $args = array(
+#                 'title' => $cross["title"],
+#                 'description' => $cross["description"],
+#                 'begin_at' => $cross["begin_at"],
+#                 'place_line1' => $cross["place"]["line1"],
+#                 'place_line2' => $cross["place"]["line2"],
+#                 'cross_id_base62' => int_to_base62($cross_id),
+#                 'invitation_id' => $invitation["invitation_id"],
+#                 'token' => $invitation["token"],
+#                 'identity_id' => $invitation["identity_id"],
+#                 'provider' => $invitation["provider"],
+#                 'external_identity' => $invitation["external_identity"],
+#                 'name' => $invitation["name"],
+#                 'avatar_file_name' => $invitation["avatar_file_name"]
+#         );
+#
+#        require 'lib/Resque.php';
+#        date_default_timezone_set('GMT');
+#        Resque::setBackend('127.0.0.1:6379');
+#
+#
+#        print $str;
 
     }
 
