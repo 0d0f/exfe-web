b6ac7cbcfb9b1f93ffc9d89fadeea3b8b8390d66
virushuo <virushuo@gmail.com>
2011-10-18 17:16:43 +0800

    use login via page fixed

--- views/s/welcomebox.php
+++ views/s/welcomebox.php
@@ -8,8 +8,10 @@ $displayname=$this->getVar("displayname");
 <div class="overFramel">
 <div class="overFramelogin">
 <div class="login">
+<?php if($displayname!="") {?>
 <h4>Hi <?php echo $displayname;?>!</h4>
 <h4>Welcome to <span>exfe</span> !</h4>
+<?php } ?>
 <p class="t"> An utility for hanging out with friends.  </p>
 <p class="t"> <span style="color:#0591af;">X</span> (cross),   </p>
 <p class="t"> gathering of friends for anything you want to do with bunch of people. We saves you from calling up every one just to confirm attendance, or losing in emails of endless discussion off the point. </p>
