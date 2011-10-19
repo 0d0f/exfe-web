85965bc8e67f5bb79003e7d962560471e37b5d7d
handaoliang <handaoliang@gmail.com>
2011-10-19 16:36:46 +0800

    Update: Optimize index page

--- views/home/indexView.php
+++ views/home/indexView.php
@@ -3,12 +3,34 @@
 </head>
 <body id="home">
     <?php include "share/nav.php" ?>
-    <div class="home_banner"><span></span></div>
+    <div class="home_banner" id="home_banner"><span id="x_code_img"></span></div>
     <div class="home_bottom">
-        <p class="gather_btn">
+        <div class="gather_btn">
             <a href="/x/gather"><img src="/static/images/home_gather_btn.jpg" alt="Gather" title="Gather" /></a>
-        </p>
-        <p class="bottom_btn"></p>
+        </div>
+        <ul>
+            <li class="one"></li>
+            <li class="two"></li>
+            <li class="three"></li>
+            <li class="four"></li>
+            <li class="five"></li>
+            <li class="six"></li>
+            <li class="seven"></li>
+            <li class="eight"></li>
+            <li class="nine"></li>
+            <li class="ten"></li>
+            <li class="eleven"></li>
+        </ul>
     </div>
 </body>
+<script type="text/javascript">
+jQuery(document).ready(function(){
+    var winSize = odof.util.getWindowSize();
+    winHeight = winSize.height;
+    if(winHeight < 600){
+        jQuery("#home_banner").css({"height":"460px"});
+        jQuery("#x_code_img").css({"margin-top":"20px"});
+    }
+});
+</script>
 </html>
