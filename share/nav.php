5f5e8f9e8fb640622ddc3fef72cf6f428752965f
handaoliang <handaoliang@gmail.com>
2011-10-20 13:25:26 +0800

    Update: index images

--- share/nav.php
+++ share/nav.php
@@ -1,5 +1,5 @@
     <div id="global_header">
         <p class="logo">
-            <a href="/"><img src="/static/images/exfe_logo.jpg" alt="EXFE" title="EXFE.COM" /></a></p>
+            <a href="/"><img src="/static/images/exfe_logo.png" alt="EXFE" title="EXFE.COM" /></a></p>
         <p class="user_info" id="global_user_info"></p>
     </div>
