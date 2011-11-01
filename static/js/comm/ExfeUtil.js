586e613179094be02aa3988056eeca52a40d3ca9
Leask Huang <leask@exfe.com>
2011-11-01 15:57:52 +0800

    fixed a little bug in getDateFromString js function

--- static/js/comm/ExfeUtil.js
+++ static/js/comm/ExfeUtil.js
@@ -1042,7 +1042,7 @@ var odof = {
     util.getDateFromString = function(strTime)
     {
         var regex = /^([0-9]{2,4})-([0-1][0-9])-([0-3][0-9]) (?:([0-2][0-9]):([0-5][0-9]):([0-5][0-9]))?$/,
-            parts = strTime.replace(regex, "$1 $2 $3 $4 $5 $6").split(' ');
+            parts = (strTime.length > 10 ? strTime : (strTime + ' 00:00:00')).replace(regex, "$1 $2 $3 $4 $5 $6").split(' ');
         return new Date(parts[0], parts[1] - 1, parts[2], parts[3], parts[4], parts[5]);
     };
 
