0adf8408dfe0af4afb8af3cb9d63f56b954a1f49
handaoliang <handaoliang@gmail.com>
2011-11-22 17:51:30 +0800

    Bug fix for user login dialog

--- static/js/user/UserStatus.js
+++ static/js/user/UserStatus.js
@@ -252,6 +252,8 @@ var ns = odof.util.initNameSpace(moduleNameSpace);
 
         jQuery("#resetpwd").bind("click", function(){
             jQuery("#identity_forgot_pwd_info").html("Verification will be sent in minutes, please check your inbox.");
+            var vheight = parseInt(jQuery("#overFramel").height()-60);
+            jQuery("#identity_forgot_pwd_dialog").css({height:vheight});
             jQuery("#identity_forgot_pwd_dialog").show();
             jQuery("#f_identity").val(jQuery("#identity").val());
             jQuery("#cancel_verification_btn").bind("click",function(){
