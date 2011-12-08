0a9eaa8536652aa10c674d1b6541d91886a5ac09
virushuo <virushuo@gmail.com>
2011-12-09 02:32:03 +0800

    get conversation by timestr

--- models/ConversationModels.php
+++ models/ConversationModels.php
@@ -42,6 +42,46 @@ class ConversationModels extends DataModel{
         return $result;
     }
 
+    public function getConversationByTimeStr($postable_id,$postable_type,$updated_since="",$limit=0)
+    {
+        $sql="select * from posts where postable_id=$postable_id and postable_type='$postable_type'";
+        if($updated_since>0)
+            $sql=$sql." and created_at>'$updated_since' ";
+
+        $sql=$sql." order by updated_at desc ";
+        if($limit>0)
+            $sql=$sql." limit $limit;";
+
+        $result=$this->getAll($sql);
+        $identity=array();
+        $posts=array();
+        if ($result) {
+            foreach ($result as $post) {
+                $identity_id=$post["identity_id"];
+                if ($identity[$identity_id] == "") {
+                    $sql="select provider,external_identity,external_username,name,bio,avatar_file_name from identities where id=$identity_id;";
+                    $identity=$this->getRow($sql);
+                    if ($identity) {
+                        $sql="select name,avatar_file_name from users,user_identity where users.id=user_identity.userid and user_identity.identityid=$identity_id";
+                        $user=$this->getRow($sql);
+                        $humanidentity=humanIdentity($identity,$user);
+                        $post["identity"]=$humanidentity;
+                        $identity[$identity_id]=$humanidentity;
+                    }
+                } else {
+                    $post["identity"]=$identity[$identity_id];
+                }
+                // @todo: time fixed
+                $strDate = date('Y-m-d H:i:s', strtotime($post['created_at']) );
+                $post['created_at']=$strDate;
+                $post['updated_at']=$strDate;
+                array_push($posts,$post);
+            }
+        }
+        return $posts;
+
+    }
+
     public function getConversation($postable_id,$postable_type,$updated_since=0,$limit=0)
     {
         $sql="select * from posts where postable_id=$postable_id and postable_type='$postable_type'";
