47836503cf75044f3e2137565bd21a790e23342a
Leask Huang <leask@exfe.com>
2012-01-13 20:53:22 +0800

    finished task #1035

--- static/js/apps/exfee.js
+++ static/js/apps/exfee.js
@@ -12,6 +12,8 @@ var ns = odof.util.initNameSpace(moduleNameSpace);
 
 (function(ns) {
 
+    ns.exfeeAvailableIdK = 'exfee_available_for_id' 
+
     ns.exfeeAvailableKey = 'exfee_available';
 
     ns.arrStrRsvp        = ['Not responded', 'Accepted', 'Declined', 'Interested'];
@@ -97,8 +99,11 @@ var ns = odof.util.initNameSpace(moduleNameSpace);
         this.timerBaseInfo[domId] = {};
         $('#' + domId).html(strHtml);
         if (typeof localStorage !== 'undefined') {
+            var curIdentity = myIdentity && typeof myIdentity.external_identity !== 'undefined'
+                            ? myIdentity.external_identity.toLowerCase() : null;
             this.exfeeAvailable = localStorage.getItem(this.exfeeAvailableKey);
-            if (this.exfeeAvailable) {
+            if (localStorage.getItem(this.exfeeAvailableIdK) === curIdentity
+             && this.exfeeAvailable) {
                 try {
                     this.exfeeAvailable = JSON.parse(this.exfeeAvailable);
                 } catch (err) {
@@ -786,6 +791,9 @@ var ns = odof.util.initNameSpace(moduleNameSpace);
             this.exfeeIdentified[curIdentity] = true;
         }
         if (typeof localStorage !== 'undefined') {
+            var curIdentity = myIdentity && typeof myIdentity.external_identity !== 'undefined'
+                            ? myIdentity.external_identity.toLowerCase() : '';
+            localStorage.setItem(this.exfeeAvailableIdK, curIdentity);
             localStorage.setItem(this.exfeeAvailableKey,
                                  JSON.stringify(this.exfeeAvailable));
         }
