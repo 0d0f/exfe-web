a8e6ffcbb5a2a9aaa2f5e05d549968cc6ce9c505
virushuo <virushuo@gmail.com>
2012-02-07 23:30:47 +0800

    generate the identity object in the rsvp update api by to_id

--- helpers/SHelper.php
+++ helpers/SHelper.php
@@ -189,13 +189,13 @@ class SHelper extends ActionController
                             if (!isset($cleanLogs[$xlogsHash[$xId]][$action])) {
                                 $cleanLogs[$xlogsHash[$xId]][$action] = array();
                             }
+                            $exfee_userid=$modUser->getUserIdByIdentityId($toExfee);
                             if ($complexobject === true) {
-                                $myidentity = $modIdentity->getIdentityById($logItem['to_id']);
-                                $user = $modUser->getUserByIdentityId($logItem['to_id']);
+                                $myidentity = $modIdentity->getIdentityById($toExfee);
+                                $user = $modUser->getUserByIdentityId($exfee_userid);
                                 $identity = humanIdentity($myidentity, $user);
                             }
 
-                            $exfee_userid=$modUser->getUserIdByIdentityId($toExfee);
                             array_push(
                                 $cleanLogs[$xlogsHash[$xId]][$action],
                                 array(
