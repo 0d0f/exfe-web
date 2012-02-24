0019453d695a6b3651df4e686c3ed1a513f8c30b
Leask Huang <leask@exfe.com>
2012-02-24 14:42:47 +0800

    finished task #1158 and #1160, 2nd

---
+++ upgrade_place_db_2nd.php
@@ -0,0 +1,25 @@
+/**
+ * http://code.google.com/intl/en-us/apis/maps/articles/phpsqlajax.html
+ * by @Leaskh
+ */
+
+<?php
+error_reporting(E_ALL ^ E_NOTICE);
+
+require_once dirname(__FILE__)."/common.php";
+require_once dirname(__FILE__)."/DataModel.php";
+
+class UpgradePlaceDB extends DataModel{
+
+    public function run(){
+        $sql = 'ALTER TABLE `places`
+                CHANGE `lng` `lng` FLOAT (10, 6) NOT NULL,
+                CHANGE `lat` `lat` FLOAT (10, 6) NOT NULL;';
+        $this->query($sql);
+        echo "upgrade success....\r\n";
+    }
+}
+
+$upgradeObj = new UpgradePlaceDB();
+
+$upgradeObj->run();
