3516bcb1b263f2c9e8e74d0882fdc38d2aec0500
cfd <cfd@exfe.com>
2012-03-09 15:31:45 +0800

    change bold tag

--- static/js/apps/x.js
+++ static/js/apps/x.js
@@ -61,16 +61,15 @@ var moduleNameSpace = 'odof.x.render',
         var str = '';
         switch (myrsvp) {
             case 1:
-                str = 'Confirmed by <b>' + username + '</b>.';
-            break;
+                str = 'Confirmed by <span class="bold">' + username + '</span>.';
+                break;
             case 2:
-                str = 'Declined by <b>' + username + '</b>.';
-            break;
+                str = 'Declined by <span class="bold">' + username + '</span>.';
+                break;
             case 3:
                 break;
             case 0:
                 str = 'Request invitation';
-
         }
         return str;
     }
@@ -213,7 +212,6 @@ var moduleNameSpace = 'odof.x.render',
                       +     '<div id="x_conversation_input_area" class="cleanup">'
                       +         '<img id="x_conversation_my_avatar" class="x_conversation_avatar">'
                       +         '<textarea id="x_conversation_input"></textarea>'
-                      //+         '<input id="x_conversation_submit" type="button" title="Say!">'
                       +     '</div>'
                       +     '<ol id="x_conversation_list"></ol>'
                       + '</div>'
