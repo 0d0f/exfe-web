733b4d73affbdefbe0dd9540e27e597a2b26da27
Leask Huang <leask@exfe.com>
2012-03-14 19:08:37 +0800

    finished task #1256

--- models/BackgroundModels.php
+++ models/BackgroundModels.php
@@ -5,7 +5,7 @@ class BackgroundModels extends DataModel {
     public function getAllBackground() {
         $sql    = 'SELECT `image` FROM `background`;';
         $images = $this->getAll($sql);
-        
+
         $result = array();
         foreach ($images as $iI => $iItem) {
             $result[] = $iItem['image'];
