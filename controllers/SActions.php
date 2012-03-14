d5018ef0e18f9bb19cf3657570ecf6b6a04c2001
handaoliang <handaoliang@gmail.com>
2012-03-14 15:45:34 +0800

    bugfix for issue: #1218

--- controllers/SActions.php
+++ controllers/SActions.php
@@ -639,24 +639,9 @@ class SActions extends ActionController {
 
     public function doLogout()
     {
-        unset($_SESSION["userid"]);
-        unset($_SESSION["identity_id"]);
-        unset($_SESSION["identity"]);
-        unset($_SESSION["tokenIdentity"]);
-        session_destroy();
-
-        unset($_COOKIE["uid"]);
-        unset($_COOKIE["id"]);
-        unset($_COOKIE["loginsequ"]);
-        unset($_COOKIE["logintoken"]);
-
-        setcookie('uid', NULL, -1,"/",COOKIES_DOMAIN);
-        setcookie('id', NULL, -1,"/",COOKIES_DOMAIN);
-        setcookie('loginsequ', NULL,-1,"/",COOKIES_DOMAIN);
-        setcookie('logintoken',NULL,-1,"/",COOKIES_DOMAIN);
-
+        $userData = $this->getModelByName("user");
+        $userData->doDestroySessionAndCookies();
         header('location:/');
-
     }
 
     public function doLogin()
