94d929f09cb7b5c56e9c8e271ae301fe1046f87a
Leask Huang <leask@exfe.com>
2012-03-25 18:45:00 +0800

    finished task #1327

--- helpers/MsgHelper.php
+++ helpers/MsgHelper.php
@@ -6,6 +6,8 @@ class MsgHelper extends ActionController {
             require_once 'lib/Resque.php';
             date_default_timezone_set('GMT');
             Resque::setBackend(RESQUE_SERVER);
+            /////////////////////////////////////
+            print_r($mail);
             $jobId = Resque::enqueue("conversationemail","conversationemail_job" , $mail, true);
             return $jobId;
     }
@@ -52,6 +54,3 @@ class MsgHelper extends ActionController {
     }
 
 }
-
-
-
