0357039b31fb09c03cb5787b8a637a6a04aee2e4
virushuo <virushuo@gmail.com>
2012-03-27 17:47:02 +0800

    new pem cert

--- FrontController.php
+++ FrontController.php
@@ -4,53 +4,6 @@ require_once dirname(__FILE__)."/DataModel.php";
 
 class FrontController {
 
-#	public static function checkOauthToken() {
-#
-#	if(isset($_GET["magic_token"])) {
-#
-#		$_SESSION["userid"] = $_GET["magic_token"];
-#
-#		return;
-#
-#	} else {
-#
-#		$accesstoken = '';
-#
-#		if(isset($_GET['oauth_token']) ) {
-#			$accesstoken=$_GET['oauth_token'];
-#		}
-#		else if(isset($_GET['access_token']) ) {
-#			$accesstoken=$_GET['access_token'];
-#		}
-#
-#		if ($accesstoken=="") {
-#
-#			$responobj["meta"]["code"]=400;
-#			$responobj["meta"]["errType"]="Bad Request";
-#			$responobj["meta"]["errorDetail"]="invalid_auth";
-#			echo json_encode($responobj);
-#			exit();
-#
-#		} else {
-#
-#			$name="user";
-#	    		$class = ucfirst($name) . "Models";
-#	    		$modelfile = MODEL_DIR . "/" . $name. "/" . $class.  ".php";
-#	    		include_once $modelfile;
-#	    		$userData=new $class;
-#			$useruuid=$userData->getUserIdByAccessToken($accesstoken);
-#			if(intval($useruuid)>0) {
-#				$_SESSION["userid"]=$useruuid;
-#			} else {
-#				$responobj["meta"]["code"]=401;
-#				$responobj["meta"]["errType"]="invalid_auth";
-#				$responobj["meta"]["errorDetail"]="invalid_auth";
-#				echo json_encode($responobj);
-#				exit();
-#			 }
-#		}
-#	}
-#	}
 
     public static function createInstance() {
         if (!defined("PAGE_DIR")) {
@@ -65,10 +18,6 @@ class FrontController {
         $action = !empty($_GET["action"]) ? $_GET["action"] : "index";
 
         $class = ucfirst($classname) . "Actions";
-
-#if(!($class=="UserActions" && ($action=="loginweb" || $action=="auth" || $action == 'register'))) {
-#	$this->checkOauthToken();
-#}
 
         $file = CONTROLLER_DIR. "/" . $class . ".php";
         if (!is_file($file)) {
