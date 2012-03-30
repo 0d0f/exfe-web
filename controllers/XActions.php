401ce14323d38dd68aaa21665aee4a3f9ab93076
Leask Huang <leask@exfe.com>
2012-03-30 14:27:58 +0800

    fix a bug in read only mode of cross

--- controllers/XActions.php
+++ controllers/XActions.php
@@ -166,13 +166,6 @@ class XActions extends ActionController
         $ehelper   = $this->getHelperByName('exfee');
         $exfees_list = $ehelper->addExfeeIdentify($cross_id, $exfees, $identity_id, $idents);
 
-        /**
-         * DONT send exfee changes currently
-        if (isset($_POST['exfee_only']) && $_POST['exfee_only']) {
-            return;
-        }
-        */
-
         $newExfees=$exfees_list["newexfees"];
         $allExfees=$exfees_list["allexfees"];
         $delExfees=$exfees_list["delexfees"];
