ca3a5fe0f101b61806a3f4933e6286ebe4c99378
virushuo <virushuo@gmail.com>
2012-04-03 20:52:03 +0800

    build base class:EFObject

---
+++ Classes/EFObject.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'Metainfo.php';
+require_once 'Cross.php';
+
+abstract class EFobject{
+    
+    public $id          = null;
+    public $type        = null;
+    
+    public function __construct() {
+    }
+
+}
+
