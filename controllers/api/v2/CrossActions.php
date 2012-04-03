145f7b240221b6606e36f64cb2d761fbe319b16d
virushuo <virushuo@gmail.com>
2012-04-03 18:37:05 +0800

    v2 model and api

---
+++ controllers/api/v2/CrossActions.php
@@ -0,0 +1,40 @@
+<?php
+
+class CrossActions extends ActionController {
+
+    public function doIndex()
+    {
+        $params=$this->params;
+        $crossData=$this->getModelByName("cross","v2");
+        $cross=$crossData->getCross(100092);
+
+//category
+//created_at
+//by_identity
+
+
+       
+        $cross["type"]="cross";
+        $cross["relation"]=array("id"=>"","type"=>"");
+        $cross["widget"]["background"]=$cross["background"];
+        $cross["widget"]["theme"]="";
+        $cross["by_identity_id"]=$cross["host_id"];
+        unset($cross["background"]);
+        print_r($cross);
+
+
+
+
+
+        //time
+        //place
+        //attribute
+        //exfee_id
+        //widget[
+        //background
+        //theme
+        //]
+        
+    }
+
+}
