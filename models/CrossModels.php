806c9ded3a01380bc99023ce5e196f91e395f98d
Leask Huang <leask@exfe.com>
2012-04-03 18:07:27 +0800

    working on the new api

---
+++ models/CrossModels.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'cross.php';
+
+class CrossModels extends DataModel {
+
+    public function new($cross) {
+
+    }
+
+    public function edit($cross) {
+
+    }
+
+    public function updated($since) {
+
+    }
+
+}
---
+++ post.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'meta_info.php';
+
+class post extends meta_info {
+
+    public $content     = null;
+
+    public $exfee_id    = null;
+
+    public function __construct() {
+        parent::__construct();
+        $this->category = 'post';
+    }
+
+}
