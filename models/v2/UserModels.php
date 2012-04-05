fd49a38a25291d419795f079317aa4076196af77
Leask Huang <leask@exfe.com>
2012-04-05 14:41:13 +0800

    rname the classes

---
+++ models/v2/UserModels.php
@@ -0,0 +1,11 @@
+<?php
+
+class CrossModels extends DataModel {
+    public function getCross($crossid)
+    {
+        $sql="select * from crosses where id=$crossid";
+        $result=$this->getRow($sql);
+        return $result;
+    }
+
+}
