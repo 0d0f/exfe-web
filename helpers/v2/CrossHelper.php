501797c6af7b6535b476d2a0040628c53d298449
virushuo <virushuo@gmail.com>
2012-04-06 22:49:45 +0800

    compatible cross time

--- helpers/v2/CrossHelper.php
+++ helpers/v2/CrossHelper.php
@@ -16,7 +16,20 @@ class CrossHelper extends ActionController {
         $identityData=$this->getModelByName("identity","v2");
         $by_identity=$identityData->getIdentityById($cross["host_id"]);
         $background=new Background($cross["background"]);
-        $begin_at=new CrossTime("","","","","","","");
+        if(sizeof(explode(" ",$cross["begin_at"]))>1)
+        {
+            $datetime=explode(" ",$cross["begin_at"]);
+            $date=$datetime[0];
+            $time=$datetime[1];
+        }
+        $begin_at=new CrossTime("",$date,"",$time,$cross["timezone"],$cross["origin_begin_at"],"");
+        ///($date_word,$date,$time_word,$time,$timezone,$origin,$originMark)
+
+        $attribute=array();
+        if($cross["state"]==1)
+            $attribute["state"]="published";
+        else if($cross["state"]==0)
+            $attribute["state"]="draft";
 
         $cross=new Cross($cross_id,$cross["title"], $cross["description"], $attribute, $exfee_id, array($background),$begin_at, $place);
 
