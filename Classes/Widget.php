cb9b211b197364f750098cbc64157d28fb777b25
virushuo <virushuo@gmail.com>
2012-04-06 20:46:40 +0800

    EFTime/CrossTime

--- Classes/Widget.php
+++ Classes/Widget.php
@@ -6,7 +6,7 @@ abstract class Widget extends EFObject{
     public $widget_id;
 
     public function __construct($widget_id) {
-        $this->type="widget";
+        parent::__construct($widget_id,"Widget");
         $this->widget_id=$widget_id;
     }
 
