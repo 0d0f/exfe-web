63f06afa063ffe89a1777fc3c3b6cc055779c702
Leask Huang <leask@exfe.com>
2012-04-11 17:07:51 +0800

    working on addInvitationIntoExfee

--- models/v2/ExfeeModels.php
+++ models/v2/ExfeeModels.php
@@ -28,13 +28,41 @@ class ExfeeModels extends DataModel {
     }
     
     
-    public function addExfeeById($id, $exfee) {
-        
+    public function addInvitationIntoExfee($invitation, $exfee_id) {
+        if (!$invitation->identity->id) {
+            
+        }
+        // make invitation token
+        $invToken = md5(base64_encode(pack('N6', mt_rand(), mt_rand(), mt_rand(), mt_rand(), mt_rand(), uniqid())));
+        // insert invitation into database
+        return $this->query(
+            'INSERT INTO `invitations` SET'
+          . '`identity_id`    = ' . $invitation->identity->id . ','
+          . '`cross_id`       = ' . $exfee_id                 . ','
+          . '`state`          = ' . $invitation->rsvp_status  . ','
+          . '`created_at`     = NOW(),'
+          . '`updated_at`     = NOW(),'
+          . "`token`          = '{$invToken}',"
+          . '`by_identity_id` = ' . $invitation->by_identity->id;        
+        );
     }
 
 
-    public function updateExfeeById($id) {
-    
+    public function addExfee($invitations = array()) {
+        $dbResult = $this->query("INSERT INTO `exfees` SET `id` = 0");
+        $id       = intval($dbResult['insert_id']);
+        foreach ($invitations as $iI => $iItem) {
+            $this->addExfee($iItem, $exfee_id);
+        }
+    }
+
+
+    public function updateExfeeById($id, $invitations = array()) {
+        if (!$id) {
+            return null;
+        }
+        $this->query("DELETE FROM `invitations` WHERE `cross_id` = {$id}");
+        return $this->addExfee($id, $invitations);
     }
 
 }
