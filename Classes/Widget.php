36b6ed13330827b4d39269387b39495775914053
Leask Huang <leask@exfe.com>
2012-04-12 12:19:51 +0800

    fixed a bug in packageIdentity

--- Classes/Widget.php
+++ Classes/Widget.php
@@ -6,7 +6,7 @@ abstract class Widget extends EFObject{
     public $widget_id;
 
     public function __construct($widget_id) {
-        parent::__construct($widget_id,"Widget");
+        parent::__construct($widget_id, "Widget");
         $this->widget_id=$widget_id;
     }
 
