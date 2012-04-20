9960e493091a380ac256b139cdf8f4c3df30a897
Leask Huang <leask@exfe.com>
2012-04-20 15:58:47 +0800

    upgraded update_static_code_timestamp.sh

--- update_static_code_timestamp.sh
+++ update_static_code_timestamp.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-sed -i '' -e "s/.*STATIC_CODE_TIMESTAMP.*/define\("STATIC_CODE_TIMESTAMP", `date +%s`\)\;/" config.php
+
+case "`uname -s`" in
+    'Darwin')
+        sed -i '' -e "s/.*STATIC_CODE_TIMESTAMP.*/define\("STATIC_CODE_TIMESTAMP", `date +%s`\)\;/" config.php
+    ;;
+    'FreeBSD')
+        sed -i '' -e "s/.*STATIC_CODE_TIMESTAMP.*/define\("STATIC_CODE_TIMESTAMP", `date +%s`\)\;/" config.php
+    ;;
+    *)
+        sed -i "s/.*STATIC_CODE_TIMESTAMP.*/define\("STATIC_CODE_TIMESTAMP", `date +%s`\)\;/" config.php
+esac
+
 echo 'done :)'
