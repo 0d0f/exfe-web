711878d35bad80ad211068ad677fb3acda8d0b21
Leask Huang <i@leaskh.com>
2012-04-23 16:24:50 +0800

    upgrade exfee model adding field exfee_updated_at

--- Classes/Invitation.php
+++ Classes/Invitation.php
@@ -1,38 +1,42 @@
 <?php
 class Invitation extends EFObject {
 
-    public $identity    = null;
+    public $identity         = null;
     
-    public $by_identity = null;
+    public $by_identity      = null;
     
-    public $rsvp_status = null;
+    public $rsvp_status      = null;
         
-    public $via         = null;
+    public $via              = null;
     
-    public $created_at  = null;
+    public $created_at       = null;
     
-    public $updated_at  = null;
+    public $updated_at       = null;
     
-    public $token       = null;
+    public $exfee_updated_at = NOW(),
+    
+    public $token            = null;
 
 
-    public function __construct($id          = 0,
-                                $identity    = null,
-                                $by_identity = null,
-                                $rsvp_status = '',
-                                $via         = '',
-                                $token       = '',
-                                $created_at  = '',
-                                $updated_at  = '') {
+    public function __construct($id               = 0,
+                                $identity         = null,
+                                $by_identity      = null,
+                                $rsvp_status      = '',
+                                $via              = '',
+                                $token            = '',
+                                $created_at       = '',
+                                $updated_at       = '',
+                                $exfee_updated_at = '') {
         parent::__construct($id, 'invitation');
         
-        $this->identity    = $identity;
-        $this->by_identity = $by_identity;
-        $this->rsvp_status = $rsvp_status;
-        $this->via         = $via;
-        $this->token       = $token;
-        $this->created_at  = $created_at;
-        $this->updated_at  = $updated_at;
+        $this->identity         = $identity;
+        $this->by_identity      = $by_identity;
+        $this->rsvp_status      = $rsvp_status;
+        $this->via              = $via;
+        $this->token            = $token;
+        $this->created_at       = $created_at;
+        $this->updated_at       = $updated_at;
+        $this->exfee_updated_at = $exfee_updated_at;
     }
 
 }
