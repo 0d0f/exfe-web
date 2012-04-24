256fb1d6d41a7b97ccbcee5cda76b4bc04adb238
virushuo <virushuo@gmail.com>
2012-04-24 17:38:32 +0800

    api allow check

--- doc/upgrade_db_script/20120423_alter_intivation_exfee.sql
+++ doc/upgrade_db_script/20120423_alter_intivation_exfee.sql
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 ALTER TABLE  `invitations` ADD  `exfee_updated_at` DATETIME NOT NULL;
+=======
+ALTER TABLE  `invitations` ADD  `exfee_updated_at` DATETIME NOT NULL ;
+ALTER TABLE  `crosses` ADD  `exfee_id` bigint(20) NOT NULL ;
+>>>>>>> api allow check
 
 ALTER TABLE  `invitations` ADD INDEX (`exfee_updated_at`);
 
