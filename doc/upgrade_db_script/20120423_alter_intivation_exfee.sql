152ee8873ec91b0dc795e2854d7dc1a625447be7
Leask Huang <i@leaskh.com>
2012-04-24 16:12:48 +0800

    upgrade some database script

--- doc/upgrade_db_script/20120423_alter_intivation_exfee.sql
+++ doc/upgrade_db_script/20120423_alter_intivation_exfee.sql
@@ -1,5 +1,5 @@
-ALTER TABLE  `invitations` ADD  `exfee_updated_at` DATETIME NOT NULL ;
+ALTER TABLE  `invitations` ADD  `exfee_updated_at` DATETIME NOT NULL;
 
-ALTER TABLE  `invitations` ADD INDEX (  `exfee_updated_at` ) ;
+ALTER TABLE  `invitations` ADD INDEX (`exfee_updated_at`);
 
-ALTER TABLE  `exfees` DROP  `updated_at`
+ALTER TABLE  `exfees` DROP  `updated_at`;
