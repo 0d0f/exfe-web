07551bdada82d5aa43e8a7892b12cffc7af86e8b
Leask Huang <i@leaskh.com>
2012-04-24 11:24:29 +0800

    fixed a but in UserActions.php v2

--- controllers/api/v2/UserActions.php
+++ controllers/api/v2/UserActions.php
@@ -52,8 +52,8 @@ class UserActions extends ActionController {
         if (!$modUser->verifyUserPassword($user_id, $password)) {
             apiError(403, 'invalid_password', ''); // 密码错误
         }
-        if ($modIdentity->addIdentity($provider, $external_id, $userID = 0)) {
-            apiResponse(array('user_id' => $user_id, 'is_new_identity' => $isNewIdentity));
+        if ($identity_id = $modIdentity->addIdentity($provider, $external_id, $user_id)) {
+            apiResponse(array('user_id' => $user_id, 'identity_id' => $identity_id));
         } else {
             apiError(400, 'failed', '');
         }
@@ -253,11 +253,11 @@ class UserActions extends ActionController {
         if (!($user_id = $_SESSION['signin_user']->id || $_SESSION['userid'])) { // @todo removing $_SESSION['userid']
             apiError(401, 'no_signin', ''); // 需要登录
         }
-        if (!($curPassword = $_POST['current_password'])) {
-            apiError(401, 'no_current_password', ''); // 请输入当前密码
-        }
+        // if (!($curPassword = $_POST['current_password'])) {
+        //     apiError(401, 'no_current_password', ''); // 请输入当前密码
+        // }
         if (!$modUser->verifyUserPassword($user_id, $curPassword, true)) {
-            apiError(403, 'invalid_password', ''); // 密码错误
+            apiError(403, 'invalid_current_password', ''); // 密码错误
         }
         if (!($newPassword = $_POST['new_password'])) {
             apiError(400, 'no_new_password', ''); // 请输入当新密码
