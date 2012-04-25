861efe808bfa8f191bdfceb850d78af98516dd57
virushuo <virushuo@gmail.com>
2012-04-25 18:28:14 +0800

    bug fixed for cross api, and add update info to redis

--- controllers/api/v2/ExfeeActions.php
+++ controllers/api/v2/ExfeeActions.php
@@ -24,15 +24,4 @@ class ExfeeActions extends ActionController {
         }
         echo json_encode(array('error' => 500));
     }
-    public function doUpdate(){
-        $params   = $this->params;
-        $exfee_id=$params["id"];
-        $updated_at=$params["updated_at"];
-
-        $exfeeHelper = $this->getHelperByName('exfee', 'v2');
-        $update=$exfeeHelper->getUpdate($exfee_id,$updated_at);
-        print_r($update);
-
-    }
-
 }
--- models/v2/CrossModels.php
+++ models/v2/CrossModels.php
@@ -14,16 +14,17 @@ class CrossModels extends DataModel {
         $result=$this->getRow($sql);
         return $result;
     }
-    public function addCross($cross,$place_id=0,$exfee_id=0)
+    public function addCross($cross,$place_id=0,$exfee_id=0,$by_identity_id=0)
     {
         $cross_time=$cross->time;
         $widgets=$cross->widget;
         $background="";
-        foreach($widgets as $widget)
-        {
-            if($widget->type==="Background")
-                $background=$widget->image;
-        }
+        if($widgets)
+            foreach($widgets as $widget)
+            {
+                if($widget->type==="Background")
+                    $background=$widget->image;
+            }
 
         
         $begin_at_time_in_old_format=$cross_time->begin_at->date." ".$cross_time->begin_at->time;
@@ -33,7 +34,7 @@ class CrossModels extends DataModel {
         {
             $sql = "insert into crosses (host_id, created_at, time_type, updated_at,
                 state, title, description,exfee_id, begin_at, place_id,
-                timezone, origin_begin_at, background,date_word,time_word,`date`,`time`,outputformat) values({$cross->by_identity_id}, NOW(),
+                timezone, origin_begin_at, background,date_word,time_word,`date`,`time`,outputformat) values({$cross->host_id}, NOW(),
                 '{$time_type}', NOW(), '1', '{$cross->title}',
                 '{$cross->description}',{$exfee_id},'{$begin_at_time_in_old_format}',
                 {$place_id}, '{$cross_time->begin_at->timezone}',
@@ -44,11 +45,58 @@ class CrossModels extends DataModel {
             return $cross_id;
         }
         else {
-            $sql="update crosses set host_id={$cross->by_identity_id},  updated_at=now(),title='{$cross->title}', description='{$cross->description}', begin_at='{$cross_time->begin_at->date_word}', place_id=$place_id, timezone='{$cross_time->begin_at->timezone}', origin_begin_at='{$cross_time->origin}', background='{$background}',date_word='{$cross_time->begin_at->date_word}',time_word='{$cross_time->begin_at->time_word}',`date`='{$cross_time->begin_at->date}',`time`='{$cross_time->begin_at->time}',outputformat='{$cross_time->outputformat}' where `id`=$cross->id;";
+            $updatefields=array();
+            $cross_updated=array();
+            
+            $updated=array("updated_at"=>time(),"identity_id"=>$by_identity_id);
+            if($place_id>0) 
+            {
+                array_push($updatefields,"place_id=$place_id");
+                $cross_updated["place"]=$updated;
+            }
+            if($cross->title)
+            {
+                array_push($updatefields,"title='{$cross->title}'");
+                $cross_updated["title"]=$updated;
+            }
+            if($cross->description)
+            {
+                array_push($updatefields,"description='{$cross->description}'");
+                $cross_updated["description"]=$updated;
+            }
+            if($cross_time)
+            {
+                array_push($updatefields,"begin_at='{$begin_at_time_in_old_format}'");
+                array_push($updatefields,"date_word='{$cross_time->begin_at->date_word}'");
+                array_push($updatefields,"time_word='{$cross_time->begin_at->time_word}'");
+                array_push($updatefields,"`date`='{$cross_time->begin_at->date}'");
+                array_push($updatefields,"`time`='{$cross_time->begin_at->time}'");
+                array_push($updatefields,"outputformat='{$cross_time->outputformat}'");
+                array_push($updatefields,"timezone='{$cross_time->begin_at->timezone}'");
+                array_push($updatefields,"origin_begin_at='{$cross_time->origin}'");
+                $cross_updated["time"]=$updated;
+            }
+            if($background)
+            {
+                array_push($updatefields,"background='{$background}'");
+                $cross_updated["background"]=$updated;
+            }
+            if($cross->host_id)
+            {
+                array_push($updatefields,"host_id={$cross->host_id}'");
+                $cross_updated["host"]=$updated;
+            }
+                
+            $updatesql=implode($updatefields,",");
 
-            $result   = $this->query($sql);
-            if($result   >0)
+            $sql="update crosses set updated_at=now(), $updatesql where `id`=$cross->id;";
+
+            $result = $this->query($sql);
+            if($result >0)
+            {
+                saveUpdate($cross->id,$cross_updated);
                 return $cross->id;
+            }
             else return 0;
         }
 
