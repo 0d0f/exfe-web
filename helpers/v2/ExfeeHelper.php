179eb04dc714a3e263e81e0fa759720c01e8f511
virushuo <virushuo@gmail.com>
2012-04-26 17:48:05 +0800

    add updated to user_crosse api

--- helpers/v2/ExfeeHelper.php
+++ helpers/v2/ExfeeHelper.php
@@ -1,10 +1,10 @@
 <?php
 
 class ExfeeHelper extends ActionController {
-    public function getExfeeIdByUserid($userid)
+    public function getExfeeIdByUserid($userid,$updated_at)
     {
         $exfeeData = $this->getModelByName('exfee', 'v2');
-        $exfee_id_list= $exfeeData->getExfeeIdByUserid($userid);
+        $exfee_id_list= $exfeeData->getExfeeIdByUserid($userid,$updated_at);
         return $exfee_id_list;
     }
     public function getUpdate($exfee_id,$updated_at)
