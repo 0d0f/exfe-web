a6336d06f0076755762a4ba72d0a9b716c45484c
virushuo <virushuo@gmail.com>
2012-04-27 17:39:07 +0800

    don't send change email to myself

--- helpers/ExfeeHelper.php
+++ helpers/ExfeeHelper.php
@@ -166,6 +166,8 @@ class ExfeeHelper extends ActionController {
                 if (intval($invitation["by_identity_id"]) > 0) {
                     $userprofile=$userData->getUserProfileByIdentityId($invitation["identity_id"]);
                 }
+                if($invitation["by_identity_id"]===$invitation["identity_id"]) //don't send to myself
+                    continue;
                 $by_identity=$identitydata->getIdentityById($invitation["by_identity_id"]);
                 $to_identity=$identitydata->getIdentityById($invitation["identity_id"]);
                 $args = array(
