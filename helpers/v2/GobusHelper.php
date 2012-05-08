97fd115729e6a6b1e6ff40800c8ffdea2d887a72
Leask Huang <i@leaskh.com>
2012-05-08 14:16:44 +0800

    removed old code

--- helpers/v2/GobusHelper.php
+++ helpers/v2/GobusHelper.php
@@ -1,6 +1,5 @@
 <?php
 date_default_timezone_set('GMT');
-//require_once 'lib/Redisent/Redisent.php';
 
 
 class GobusHelper extends ActionController {
@@ -12,8 +11,6 @@ class GobusHelper extends ActionController {
         $this->redis = new Redis();
         $this->redis->connect(REDIS_SERVER_ADDRESS, REDIS_SERVER_PORT);
     }
-    
-    
 
 
     public function send($queue_name, $method, $arg, $max_retry = 5) {
