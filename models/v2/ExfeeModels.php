2ffa454df69cf9823f7ed1d3dd3e9565edc15f8e
Leask Huang <i@leaskh.com>
2012-05-10 11:51:21 +0800

    changed to_identity to to_invitation

--- models/v2/ExfeeModels.php
+++ models/v2/ExfeeModels.php
@@ -138,7 +138,7 @@ class ExfeeModels extends DataModel {
             }
         }
         foreach ($cross->exfee->invitations as $invitation) {
-            $msgArg['to_identity'] = $invitation->identity;
+            $msgArg['to_invitation'] = $invitation;
             $hlpGobus->send("{$invitation->identity->provider}_job", 'update_exfee', $msgArg);
         }
     }
