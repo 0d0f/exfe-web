796c8ccbf90f7455ebd417336331ec99f88c8b70
Leask Huang <i@leaskh.com>
2012-05-11 15:20:33 +0800

    upgrade cross edit api for new queue schema

--- helpers/v2/UserHelper.php
+++ helpers/v2/UserHelper.php
@@ -3,18 +3,18 @@
 class UserHelper extends ActionController {
 
     protected $modUser = null;
-    
-    
+
+
     public function __construct() {
         $this->modUser = $this->getModelByName('User', 'v2');
     }
-    
+
 
     public function getUserIdsByIdentityIds($identity_ids) {
         return $this->modUser->getUserIdsByIdentityIds($identity_ids);
     }
-    
-    
+
+
     public function sendResetPasswordMail($args) {
         require 'lib/Resque.php';
         date_default_timezone_set('GMT');
