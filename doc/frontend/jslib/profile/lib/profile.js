6a762a6a483a0e539c33e64105e56c92441fbbc3
cfd <cfd@exfe.com>
2012-05-14 18:54:56 +0800

    use $.Deferr

--- doc/frontend/jslib/profile/lib/profile.js
+++ doc/frontend/jslib/profile/lib/profile.js
@@ -4,79 +4,118 @@ define(function (require, exports, module) {
   var Handlebars = require('handlebars');
   var R = require('rex');
 
-  var signin_defe = $.ajax({
-    url: 'http://api.localexfe.me/v2/users/signin',
-    type: 'POST',
-    dataType: 'JSON',
-    xhrFields: {withCredentials: true},
-    data: {
-      'external_id': 'cfd@demox.io',
-      'provder': 'email',
-      'password': 'd'
+  var signin_defe = function () {
+    var user_id = Store.get('user_id')
+      , token = Store.get('token')
+      , dfd;
+
+    // 先检查下 本地缓存
+    if (user_id && token) {
+      dfd = $.Deferred();
+      // 重新确定数据
+      dfd.resolve({
+        user_id: user_id,
+        token: token
+      });
+    } else {
+      dfd = $.ajax({
+        url: 'http://api.localexfe.me/v2/users/signin',
+        type: 'POST',
+        dataType: 'JSON',
+        xhrFields: {withCredentials: true},
+        data: {
+          'external_id': 'cfd@demox.io',
+          'provder': 'email',
+          'password': 'd'
+        }
+      }).done(function (data) {
+        if (data.meta.code === 200) {
+          user_id = data.resolve.user_id;
+          token = data.resolve.token;
+          Store.set('user_id', user_id);
+          Store.set('token', token);
+          dfd.resolve({
+            user_id: user_id,
+            token: token
+          });
+        } else {
+          alert('Sign In Fail.');
+        }
+      });
     }
-  });
+    return dfd;
+  }();
 
-  // 登陆，保存 user_id & token
-  signin_defe.done(function (data) {
-    if (data.meta.code === 200) {
-      Store.set('user_id', data.response.user_id);
-      Store.set('token', data.response.token);
-    }
-  });
-
-  var crosses_defe = function (data) {
-    var user_id = Store.get('user_id');
-    var token = Store.get('token');
-
+  // 用户信息,包括多身份信息
+  var identities_defe = function (data) {
     // 返回一个 promise 对象
     return $.ajax({
-      url: 'http://api.localexfe.me/v2/users/' + user_id + '/crosses?token=' + token,
+      url: 'http://api.localexfe.me/v2/users/' + data.user_id + '?token=' + data.token,
       type: 'GET',
       dataType: 'JSON',
       xhrFields: { withCredentials: true}
+    })
+      .done(function (data) {
+        if (data.meta.code === 200) {
+          console.dir(data);
+        }
+      });
+
+  };
+
+  // crosses 信息
+  var crosses_defe = function (data) {
+    // 返回一个 promise 对象
+    return $.ajax({
+      url: 'http://api.localexfe.me/v2/users/' + data.user_id + '/crosses?token=' + data.token,
+      type: 'GET',
+      dataType: 'JSON',
+      xhrFields: { withCredentials: true}
+    })
+      .done(function (data) {
+        if (data.meta.code === 200) {
+          var now = +new Date;
+
+          //console.dir(data.response.crosses);
+
+          var invitations = [];
+
+          // 时间倒序
+          data.response.crosses.reverse();
+          var jst_crosses = $('#jst-crosses-container');
+
+          // 注册帮助函数, 获取 `ACCEPTED` 人数
+          Handlebars.registerHelper('confirmed_nums', function (context) {
+            return R.filter(context, function (v) {
+              if (v.rsvp_status === 'ACCEPTED') return true;
+            }).length;
+          });
+
+          // 注册帮助函数, 获取总人数
+          Handlebars.registerHelper('total', function (context) {
+            return context.length;
+          });
+
+          // 注册帮助函数，列出 confirmed identities
+          Handlebars.registerHelper('confirmed_identities', function (context) {
+            return R(context).map(function (v) {
+              return v.identity.name;
+            }).join(', ');
+          });
+
+          // 注册子模版
+          Handlebars.registerPartial('jst-cross-box', $('#jst-cross-box').html())
+          // 编译模版
+          var s = Handlebars.compile(jst_crosses.html());
+          // 填充数据
+          var h = s(data.response);
+          $('.crosses').append(h);
+        }
     });
-  }();
 
-  crosses_defe.done(function (data) {
-    if (data.meta.code === 200) {
-      var now = +new Date;
+  };
 
-      //console.dir(data.response.crosses);
 
-      var invitations = [];
-
-      data.response.crosses.reverse();
-      var jst_crosses = $('#jst-crosses-container');
-
-      // 注册帮助函数, 获取 `ACCEPTED` 人数
-      Handlebars.registerHelper('confirmed_nums', function (context) {
-        return R.filter(context, function (v) {
-          if (v.rsvp_status === 'ACCEPTED') return true;
-        }).length;
-      });
-
-      // 注册帮助函数, 获取总人数
-      Handlebars.registerHelper('total', function (context) {
-        return context.length;
-      });
-
-      // 注册帮助函数，列出 confirmed identities
-      Handlebars.registerHelper('confirmed_identities', function (context) {
-        return R(context).map(function (v) {
-          return v.identity.name;
-        }).join(', ');
-      });
-
-      // 注册子模版
-      Handlebars.registerPartial('jst-cross-box', $('#jst-cross-box').html())
-      // 编译模版
-      var s = Handlebars.compile(jst_crosses.html());
-      // 填充数据
-      var h = s(data.response);
-      $('.crosses').append(h);
-    }
-  });
-
-  signin_defe.then(crosses_defe);
+  signin_defe.then([identities_defe, crosses_defe]);
 
 });
