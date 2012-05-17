f3a0f39135300950fe2d02aa132fb78e7199dde1
Leask Huang <i@leaskh.com>
2012-05-17 12:03:51 +0800

    working on merge signin and websignin, signout and websignout

--- controllers/api/v2/UsersActions.php
+++ controllers/api/v2/UsersActions.php
@@ -95,49 +95,6 @@ class UsersActions extends ActionController {
     }
 
 
-    //@todo: merge with new sign in
-    public function doWebSignin() {
-        // get models
-        $modUser       = $this->getModelByName('user',     'v2');
-        $modIdentity   = $this->getModelByName('identity', 'v2');
-        // init
-        $rtResult      = array();
-        $isNewIdentity = false;
-        // collecting post data
-        $external_id   = $_POST['external_id'];
-        $provider      = $_POST['provider'] ? $_POST['provider'] : 'email';
-        $password      = $_POST['password'];
-        $name          = $_POST['name'];
-        $autoSignin    = intval($_POST['auto_signin']) === 1;
-        // adding new identity
-        if ($external_id && $password && $name) {
-            // @todo: 根据 $provider 检查 $external_identity 有效性
-            $user_id = $modUser->newUserByPassword($password);
-            // @todo: check returns
-            $modIdentity->addIdentity($provider, $external_id, array('name' => $name), $user_id);
-            // @todo: check returns
-            $isNewIdentity = true;
-        }
-        // try to sign in
-        if ($external_id && $password && ($user_id = $modUser->login($external_id, $password, $autosignin))) {
-            apiResponse(array('user_id' => $user_id, 'is_new_identity' => $isNewIdentity));
-        } else {
-            apiError(403, 'invalid_identity_or_password', ''); // 失败
-        }
-    }
-
-
-    //@todo: merge with new sign out
-    public function doWebSignout() {
-        $modUser = $this->getModelByName('user', 'v2');
-        if ($modUser->signout()) {
-            apiResponse(array('success' => true));
-        } else {
-            apiError(400, 'failed', ''); // 失败
-        }
-    }
-
-
     public function doSetDefaultIdentity() {
         // check signin
         $checkHelper = $this->getHelperByName('check', 'v2');
@@ -188,6 +145,38 @@ class UsersActions extends ActionController {
     }
 
 
+    //@todo: merge with new sign in
+    public function doWebSignin() {
+        // get models
+        $modUser       = $this->getModelByName('user',     'v2');
+        $modIdentity   = $this->getModelByName('identity', 'v2');
+        // init
+        $rtResult      = array();
+        $isNewIdentity = false;
+        // collecting post data
+        $external_id   = $_POST['external_id'];
+        $provider      = $_POST['provider'] ? $_POST['provider'] : 'email';
+        $password      = $_POST['password'];
+        $name          = $_POST['name'];
+        $autoSignin    = intval($_POST['auto_signin']) === 1;
+        // adding new identity
+        if ($external_id && $password && $name) {
+            // @todo: 根据 $provider 检查 $external_identity 有效性
+            $user_id = $modUser->newUserByPassword($password);
+            // @todo: check returns
+            $modIdentity->addIdentity($provider, $external_id, array('name' => $name), $user_id);
+            // @todo: check returns
+            $isNewIdentity = true;
+        }
+        // try to sign in
+        if ($external_id && $password && ($user_id = $modUser->login($external_id, $password, $autosignin))) {
+            apiResponse(array('user_id' => $user_id, 'is_new_identity' => $isNewIdentity));
+        } else {
+            apiError(403, 'invalid_identity_or_password', ''); // 失败
+        }
+    }
+
+
     public function doSignout() {
         $modUser      = $this->getModelByName('user');
         $params       = $this->params;
@@ -201,6 +190,17 @@ class UsersActions extends ActionController {
             }
         }
         apiError(500, 'failed', "can't disconnect this device"); // 失败
+    }
+
+
+    //@todo: merge with new sign out
+    public function doWebSignout() {
+        $modUser = $this->getModelByName('user', 'v2');
+        if ($modUser->signout()) {
+            apiResponse(array('success' => true));
+        } else {
+            apiError(400, 'failed', ''); // 失败
+        }
     }
 
 
