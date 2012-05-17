e10c39e81ebecd8887525397f791b7e5a647db0d
Leask Huang <i@leaskh.com>
2012-05-17 19:36:07 +0800

    adding 20120517_render_default_avatar.php acript, 3rd

--- doc/upgrade_db_script/20120517_render_default_avatar.php
+++ doc/upgrade_db_script/20120517_render_default_avatar.php
@@ -18,7 +18,7 @@ class UpgradeIdentityAvatar extends DataModel {
         foreach ($ids as $id) {
             $name = $id['name'] ?: $id['external_username'];
 
-            if (preg_match('80_80_default.png', strtolower($id['avatar_file_name']))) {
+            if (preg_match('/80_80_default.png/', strtolower($id['avatar_file_name']))) {
                 echo ":) Identity {$id['id']} : {$id['external_identity']} : {$name} is already having an avatar.\r\n";
                 continue;
             }
