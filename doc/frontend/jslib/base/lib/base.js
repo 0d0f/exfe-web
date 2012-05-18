7eb63221941183d5d4d7092f50d9c8c1040b9199
cfd <cfd@exfe.com>
2012-05-18 18:43:39 +0800

    fixed bugs

--- doc/frontend/jslib/base/lib/base.js
+++ doc/frontend/jslib/base/lib/base.js
@@ -4,41 +4,17 @@ define('base', [], function (require) {
   // - https://github.com/mootools/mootools-core/blob/master/Source/Class/Class.Extras.js
   // - https://github.com/alipay/arale/blob/master/lib/base/src/options.js
 
-  var Class = require('class');
-  var Emitter = require('emitter');
-
-  return Class.create(Emitter, {
-    setOptions: function (customOptions) {
-      var key, value;
-
-      var options = this.options = merge({}, customOptions);
-
-      if (this.on) {
-        for (key in options) {
-          value = options[key];
-          if (isFunction(value) && EVENT_PREFIX.test(key)) {
-            this.on(getEventName(name), value);
-            delete options[key];
-          }
-        }
-      }
-    },
-    destory: function () {
-      var k;
-      for (k in this) {
-        if (this.hasOwnProperty(k)) {
-          delete this[p];
-        }
-      }
-    }
-  });
 
   // Helpers
   // -------
   var EVENT_PREFIX = /^on[A-Z]/;
 
+  var PROTO = Object.__proto__;
+
   var isArray = Array.isArray;
   if (!isArray) isArray = function (a) {return typeof a === 'array';}
+
+  var toString = Object.prototype.toString;
 
   function isPlainObject(o) {
     return o &&
@@ -76,5 +52,52 @@ define('base', [], function (require) {
   function getEventName(name) {
     return name[2].toLowerCase() + name.substring(3);
   }
+
+  var Class = require('class');
+  var Emitter = require('emitter');
+
+  return Class.create(Emitter, {
+
+    setOptions: function (custom) {
+      var key, value, options;
+      if (!this.hasOwnProperty('options')) {
+        this.options = {};
+      }
+
+      options = this.options;
+
+      // 父类 options
+      if (this.constructor.__super__.options) {
+        merge(options, this.constructor.__super__.options);
+      }
+
+      // 子类 options
+      if (this.constructor.prototype.options) merge(options, this.constructor.prototype.options);
+
+      // 实例 options
+      if (custom.options) merge(options, custom.options);
+
+      if (this.on) {
+        for (key in options) {
+          value = options[key];
+          if (isFunction(value) && EVENT_PREFIX.test(key)) {
+            this.on(getEventName(key), value);
+            delete options[key];
+          }
+        }
+      }
+    },
+
+    destory: function () {
+      var k;
+      for (k in this) {
+        if (this.hasOwnProperty(k)) {
+          delete this[k];
+        }
+      }
+
+      if (PROTO) this.__proto__ = PROTO;
+    }
+  });
 
 });
--- doc/frontend/jslib/widget/lib/widget.js
+++ doc/frontend/jslib/widget/lib/widget.js
@@ -5,27 +5,70 @@ define('widget', [], function (require, exports, module) {
   // UI 组件的基类，主要负责 View 层的管理
 
   var $ = require('jquery');
-  var Base = require('Base');
+  var Base = require('base');
 
   var Widget = Base.extend({
+    options: {
+      template: '<div></div>'
+    },
     // 初始化
     initialize: function (options) {
       this.cid = uuid();
       this.initOptions(options);
       this.parseElement();
+
+      this.delegateEvents();
+      this.init();
     },
 
-    initOptions: function (options) {},
+    initOptions: function (options) {
+      this.setOptions(options);
+    },
+
+    // 获取元素
+    parseElement: function () {
+      var element = this.element
+        , template = this.options.template;
+
+      if (element) {
+        this.element = element instanceof $ ? element : $(element);
+      } else if (template) {
+        this.element = $(template);
+      }
+
+      if (!this.element) {
+        throw 'element is invalid';
+      }
+    },
 
     // 外部接口，方便子类初始化
-    init: function () {},
+    init: function () {
+    },
 
     // 外部接口，将 widget 渲染到页面上
     render: function () {
       return this;
     },
 
-    delegateEvents: function (events) {},
+    delegateEvents: function (events) {
+      events || (events = getValue(this, 'events'));
+      if (!events) return;
+      this.undelegateEvents();
+
+      var key, method, match, eventName, selector;
+      for (key in events) {
+        method = this[key];
+
+        if (!method) throw 'Method "' + events[key] + '" does not exist';
+
+        match = key.match(delegateEventSplitter);
+        eventName = match[1];
+        selector = match[2] || null;
+
+        eventName += '.delegateEvents' + this.cid;
+        this.element.on(eventName, selector, proxy(method, this));
+      }
+    },
 
     undelegateEvents: function () {
       this.element.off('.delegateEvents' + this.cid);
@@ -37,17 +80,45 @@ define('widget', [], function (require, exports, module) {
     },
 
     destory: function () {
-      Widget.__super__.destory.call(this);
       this.undelegateEvents();
+      // remove `element`
+      this.element.remove();
+      Widget.__super__.destory.call(this);
     }
   });
 
-  return Widget;
 
   // Helpers
   // ------
 
-  var idCounter = 0;
-  function uuid() {return 'widget' + idCounter++;}
+  // 事件代理参数中，'event selector' 的分隔符
+  var delegateEventSplitter = /^(\S+)\s*(.*)$/;
+
+  var guid = 1;
+
+  function uuid() {
+    return 'widget-' + guid++;
+  }
+
+  function isFunction(f) {
+    return typeof f === 'function';
+  }
+
+  function getValue(o, prop) {
+    var f = o[prop];
+    if (o && f)  {
+      return isFunction(f) ? o[prop]() : f;
+    }
+  }
+
+  function proxy(fn, context) {
+    var f = function (event) {
+      return fn.call(this, event);
+    };
+    return f;
+  }
+
+
+  return Widget;
 
 });
