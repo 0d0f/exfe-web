0f861a02d184e8e92ed6e4d2b22627aa2442ad04
cfddream <cfddream@gmail.com>
2012-05-18 18:43:39 +0800

    fixed bugs

--- doc/frontend/jslib/dialog/lib/dialog.js
+++ doc/frontend/jslib/dialog/lib/dialog.js
@@ -14,7 +14,60 @@ define('dialog', [], function (require, exports, module) {
   var $ = require('jquery');
   var Widget = require('widget');
 
+  var BODY = document.body;
+
   var Dialog = Widget.extend({
 
+    options: {
+
+      template: '',
+
+      srcNode: '',
+
+      parentNode: BODY
+
+    },
+
+    init: function () {
+    },
+
+    render: function () {
+      this.parentNode = this.options.parentNode;
+      this.element.appendTo(this.parentNode);
+      this.sync();
+      return this;
+    },
+
+    sync: function () {
+      var options = this.options;
+      this.emit('sync');
+
+      this.element.css({
+        width: options.width,
+        height: options.height,
+        zIndex: options.zIndex,
+        minHeight: options.minHeight
+      });
+
+      return this;
+    },
+
+    show: function () {
+      this.emit('show');
+      this.element.show();
+      return this;
+    },
+
+    hide: function () {
+      this.element.hide();
+      this.emit('hidden');
+      return this;
+    }
+
   });
+
+  // Helper
+  // ------
+
+  return Dialog;
 });
