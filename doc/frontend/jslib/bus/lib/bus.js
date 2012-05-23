f3eb1e93ed6a42a0034fcf7ae7f5f5d1507c03d3
cfddream <cfddream@gmail.com>
2012-05-23 11:03:41 +0800

    simply bus module

---
+++ doc/frontend/jslib/bus/lib/bus.js
@@ -0,0 +1,23 @@
+define('bus', [], function (require, exports, module) {
+
+  /**
+   *
+   * 全局通信总线
+   *
+   * 模块通信的 BUS
+   *
+   * Usage:
+   *          a.js
+   *            var Bus = require('bus');
+   *            Bus.on('click', function () { ... });
+   *          b.js
+   *            var Bus = require('bus');
+   *            Bus.emit('click', data);
+   *
+   */
+
+  var Emitter = require('emitter');
+
+  return new Emitter();
+
+});
