63bb2862b779164cb0ee9d5f131fca70ccf1a21b
virushuo <virushuo@gmail.com>
2012-05-25 17:47:07 +0800

    add updated_at to conversation api

--- helpers/v2/ConversationHelper.php
+++ helpers/v2/ConversationHelper.php
@@ -20,6 +20,7 @@ class ConversationHelper extends ActionController {
         
             $conversation_post=new Post($post["id"],$identities[$identity_id],$post["content"], $post["postable_id"],$post["postable_type"],"");
             $conversation_post->created_at=$post["created_at"];
+            $conversation_post->updated_at=$post["updated_at"];
             array_push($conversation,$conversation_post);
         }
         return $conversation;
