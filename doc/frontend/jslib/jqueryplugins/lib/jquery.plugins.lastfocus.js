02119c3a9d0ab0e52987392eebad8f75937638b9
cfd <cfd@exfe.com>
2012-05-30 18:41:19 +0800

    added animation for user-panel

--- doc/frontend/jslib/jqueryplugins/lib/jquery.plugins.lastfocus.js
+++ doc/frontend/jslib/jqueryplugins/lib/jquery.plugins.lastfocus.js
@@ -6,12 +6,14 @@ define(function (require) {
 
   $.fn.lastfocus = isFF ?
     function () {
+      if (!this[0]) return;
       var target = this[0]
         , l = target.value.length;
       target.focus();
       target.setSelectionRange(l, l);
     } :
     function () {
+      if (!this[0]) return;
       this[0].focus();
     };
 
