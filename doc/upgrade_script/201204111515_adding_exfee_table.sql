381b377d4965a2adf50e17a5052d8d8d8a6258c0
Leask Huang <i@leaskh.com>
2012-06-01 14:57:39 +0800

    removed old upgrade scripts

--- doc/upgrade_script/201204111515_adding_exfee_table.sql
+++
@@ -1,4 +0,0 @@
-CREATE TABLE `exfees` (
-    `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=110000 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
