fa43a3ba9938b1fa5a1132cfcd3612964181000c
Leask Huang <i@leaskh.com>
2012-06-01 16:02:50 +0800

    format the codes

--- Classes/Metainfo.php
+++ Classes/Metainfo.php
@@ -5,12 +5,12 @@ require_once 'Place.php';
 require_once 'Post.php';
 
 abstract class Metainfo extends EFObject{
-    
-    public $relative = null;
-    public $type        = null;
-    public $created_at  = null;
-    public $by_identity = null;
-    
+
+	public $relative    = null;
+	public $type        = null;
+	public $created_at  = null;
+	public $by_identity = null;
+
     public function __construct($id=0,$type="Metainfo") {
         parent::__construct($id, $type);
         $this->relative = array();
