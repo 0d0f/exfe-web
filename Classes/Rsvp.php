4e8ef9b7e2982ede7a14bdba43c04559ce134244
Leask Huang <i@leaskh.com>
2012-06-04 17:52:42 +0800

    adding new rsvp api

---
+++ Classes/Rsvp.php
@@ -0,0 +1,21 @@
+<?php
+
+class Rsvp extends EFObject {
+
+    public $identity_id    = null;
+
+    public $rsvp_status    = null;
+
+    public $by_identity_id = null;
+
+    public function __construct($identity_id    = 0,
+                                $rsvp_status    = '',
+                                $by_identity_id = 0) {
+        parent::__construct(0, 'user');
+
+        $this->identity_id     = $identity_id;
+        $this->rsvp_status     = $rsvp_status;
+        $this->by_identity_id  = $by_identity_id;
+    }
+
+}
