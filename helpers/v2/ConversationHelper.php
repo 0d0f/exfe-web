08907c1b94a6073796ee6c1a56f45642eab735f2
virushuo <virushuo@gmail.com>
2012-06-06 15:38:25 +0800

    add timezone to conversation

--- helpers/v2/ConversationHelper.php
+++ helpers/v2/ConversationHelper.php
@@ -19,8 +19,8 @@ class ConversationHelper extends ActionController {
             }
         
             $conversation_post=new Post($post["id"],$identities[$identity_id],$post["content"], $post["postable_id"],$post["postable_type"],"");
-            $conversation_post->created_at=$post["created_at"];
-            $conversation_post->updated_at=$post["updated_at"];
+            $conversation_post->created_at=$post["created_at"]." +0000";
+            $conversation_post->updated_at=$post["updated_at"]." +0000";
             array_push($conversation,$conversation_post);
         }
         return $conversation;
