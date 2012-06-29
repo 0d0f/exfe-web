901276c7b96550104466d9be231b3b4a8ebaf598
cfd <cfd@exfe.com>
2012-06-29 16:47:03 +0800

    remvoed unless codes

--- share/header.php
+++ share/header.php
@@ -11,42 +11,10 @@
   <meta name="keywords" content="EXFE, X, cross, exfee, gather, Gather a X, hangout, gathering, invite, RSVP" />
   <meta name="description" content="EXFE, an utility for hanging out with friends." />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no" />
+  <link rel="shortcut icon" href="/static/1b/img/favicon.ico" />
+  <link rel="apple-touch-icon" href="/static/1b/img/favicon.png" />
   <title>EXFE.COM</title>
 
   <!-- V2 -->
   <link href="/static/1b/css/exfe.min.css?t=<?php echo STATIC_CODE_TIMESTAMP; ?>" rel="stylesheet" type="text/css" />
   <script src="/static/1b/js/modernizr/2.5.3/modernizr.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-
-  <!-- V1
-  <link rel="stylesheet" type="text/css" href="/static/?g=css_global&t=<?php echo STATIC_CODE_TIMESTAMP; ?>" />
-  <script type="text/javascript" src="/static/?g=js_libs&t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script type="text/javascript" src="/static/?g=js_util&t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script type="text/javascript" src="/static/js/comm/func.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script type="text/javascript" src="/static/?g=js_user&t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  -->
-
-  <script>
-      var site_url       = '<?php echo SITE_URL; ?>',
-          img_url        = '<?php echo IMG_URL; ?>',
-          cookies_domain = '<?php echo COOKIES_DOMAIN; ?>',
-          utc            =  <?php echo time(); ?>;
-  </script>
-
-  <?php
-  if($_SESSION["tokenIdentity"]!="" && $_GET["token"]!="")
-  {
-      $global_name=$_SESSION["tokenIdentity"]["identity"]["name"];
-      $global_avatar_file_name=$_SESSION["tokenIdentity"]["identity"]["avatar_file_name"];
-      $global_external_identity=$_SESSION["tokenIdentity"]["identity"]["external_identity"];
-      $global_identity_id=$_SESSION["tokenIdentity"]["identity_id"];
-
-  } else if($_SESSION["identity"]!="") {
-      $global_name=$_SESSION["identity"]["name"];
-      $global_avatar_file_name=$_SESSION["identity"]["avatar_file_name"];
-      $global_external_identity=$_SESSION["identity"]["external_identity"];
-      $global_identity_id=$_SESSION["identity_id"];
-  }
-  if(empty($global_avatar_file_name)){
-      $global_avatar_file_name = "default.png";
-  }
-  ?>
