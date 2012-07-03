bbd092864d04d429b087c3a92738cb75ee006fe0
Leask Huang <i@leaskh.com>
2012-07-03 14:54:44 +0800

    working on ExfeeWidget, 7th

--- static/1b/js/api/0.0.1/api.js
+++ static/1b/js/api/0.0.1/api.js
@@ -42,17 +42,19 @@ define('api', [], function (require, exports, module) {
     // Identity
     getIdentityById: '/identities/:identity_id',
 
+    complete: '/identities/complete',
+
     getIdentity: '/identities/get',
 
     updateIdentity: '/identities/:identity_id/update',
 
 
-    // Cross base62 id
+    // Cross
     getCross: '/crosses/:cross_id',
 
-    addCross: '/cross/add',
+    gather: '/crosses/gather',
 
-    editCross: '/cross/:cross_id/edit',
+    editCross: '/crosses/:cross_id/edit',
 
 
     // Exfee
