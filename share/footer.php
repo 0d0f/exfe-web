ff911cfc82cd8e590b36058e036761dcdd293c7e
cfddream <cfddream@gmail.com>
2012-07-04 14:18:02 +0800

    removed  1b

--- share/footer.php
+++ share/footer.php
@@ -1,29 +1,29 @@
 
-  <script src="/static/1b/js/common/0.0.1/common.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/class/0.0.1/class.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/emitter/0.0.1/emitter.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/base/0.0.1/base.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/bus/0.0.1/bus.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/rex/0.0.1/rex.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/util/0.0.1/util.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/widget/0.0.1/widget.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/common/0.0.1/common.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/class/0.0.1/class.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/emitter/0.0.1/emitter.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/base/0.0.1/base.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/bus/0.0.1/bus.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/rex/0.0.1/rex.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/util/0.0.1/util.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/widget/0.0.1/widget.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
 
-  <script src="/static/1b/js/jquery/1.7.2/jquery.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/moment/1.6.2/moment.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/store/1.3.3/store.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/handlebars/1.0.0/handlebars.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/jqfocusend/0.0.1/jqfocusend.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/jqoffset/0.0.1/jqoffset.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/jquery/1.7.2/jquery.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/moment/1.6.2/moment.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/store/1.3.3/store.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/handlebars/1.0.0/handlebars.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/jqfocusend/0.0.1/jqfocusend.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/jqoffset/0.0.1/jqoffset.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
 
   <script>
   <?php include 'ftconfig.php'; ?>
   </script>
 
-  <script src="/static/1b/js/api/0.0.1/api.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/dialog/0.0.1/dialog.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/typeahead/0.0.1/typeahead.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/xidentity/0.0.1/xidentity.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/1b/js/xdialog/0.0.1/xdialog.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/api/0.0.1/api.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/dialog/0.0.1/dialog.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/typeahead/0.0.1/typeahead.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/xidentity/0.0.1/xidentity.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+  <script src="/static/js/xdialog/0.0.1/xdialog.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
 
   <!-- 每个页面先单独引入 signin -->
-  <!--script src="/static/1b/js/userpanel/0.0.1/userpanel.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script-->
+  <!--script src="/static/js/userpanel/0.0.1/userpanel.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script-->
