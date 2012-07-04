5503a012cf77e2584cb56a7fbc55ffa388e70e2b
Leask Huang <i@leaskh.com>
2012-07-04 14:43:00 +0800

    finished the new xbgutilitie

--- xbgutilitie/xbgutilitie.php
+++ xbgutilitie/xbgutilitie.php
@@ -11,28 +11,28 @@ class xbgUtilitie extends DataModel {
     public function __construct() {
         // init
         global $curDir;
-        $this->imagePath      = "{$curDir}/../eimgs/xbgimage_origin";
+        $this->imagePath = "{$curDir}/../static/img/xbg";
     }
 
 
-    public function make() {
+    public function update() {
         // init
         global $curDir;
-        echo "Source folder: {$this->imagePath}\r\n";
+        echo "Source folder: {$this->imagePath}\r\n\r\n";
 
         // del old images
-        echo "Del old images...\r\n";
+        echo "Del old images...\r\n\r\n";
         $this->query("DELETE FROM `background`");
-        echo "\r\n";
 
         // processing images
         echo "Processing images...\r\n";
+        $ph  = opendir($this->imagePath);
         $num = 0;
         while (($file = readdir($ph))) {
             if ($file !== '.' && $file !== '..' && $file !== '.DS_Store') {
                 if (!is_dir($fullpath = "{$this->imagePath}/{$file}")) {
                     echo ++$num . ": {$fullpath}\r\n";
-                    $fileName  = preg_replace('/^.*\/([^\/]*)\.[^\.]*$/', '$1', $fullpath);
+                    $fileName  = preg_replace('/^.*\/([^\/]*)$/', '$1', $fullpath);
                     // registration
                     $this->query(
                         "INSERT INTO `background` SET `image` = '{$fileName}'"
