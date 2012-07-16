3219229e8250df92fac70335e56d4503395ab0a5
Leask Huang <i@leaskh.com>
2012-07-16 13:58:15 +0800

    upgrade home action

--- controllers/HomeActions.php
+++ controllers/HomeActions.php
@@ -1,11 +1,12 @@
 <?php
+
 class HomeActions extends ActionController {
 
     public function doIndex() {
-        if(intval($_SESSION["userid"])>0){
-            header("location:/s/profile");
-        }
+        $modBackground = $this->getModelByName('background');
+        $this->setVar('backgrounds', $modBackground->getAllBackground());
         $this->displayView();
     }
+
 }
 
