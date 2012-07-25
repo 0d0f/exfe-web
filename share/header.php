a0dd6ce9dcd109638f0861db433d2c4d4211a3ed
cfddream <cfddream@gmail.com>
2012-07-26 02:39:53 +0800

    add `media` attribute for css link

--- share/header.php
+++ share/header.php
@@ -14,5 +14,5 @@
   <link rel="shortcut icon" href="/static/img/favicon_16.png" />
   <link rel="apple-touch-icon" href="/static/img/favicon_32.png" />
   <title>EXFE.COM</title>
-  <link rel="stylesheet" type="text/css" href="/static/css/exfe.min.css?t=<?php echo STATIC_CODE_TIMESTAMP; ?>" />
+  <link rel="stylesheet" media="screen" type="text/css" href="/static/css/exfe.min.css?t=<?php echo STATIC_CODE_TIMESTAMP; ?>" />
   <script src="/static/js/modernizr/2.5.3/modernizr.js?t=<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
