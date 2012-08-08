e900c2fe5adbea52d28a365ca97c27dfb0678f49
cfd <cfd@exfe.com>
2012-08-08 18:27:17 +0800

    typo

--- static/_cross.js
+++ static/_cross.js
@@ -1764,7 +1764,7 @@ define(function (require, exports, module) {
                 UpdateCross(data.cross, false);
             },
             function(data) {
-                Bus.emit('app:cross:forbidden', cross_id);
+                bus.emit('app:cross:forbidden', cross_id);
                 console.log(data);
             }
         );
