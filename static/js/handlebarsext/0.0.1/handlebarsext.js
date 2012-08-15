f5d1e5b636f185cc0280626e1eed8d5e6f1a3ec6
cfddream <cfddream@gmail.com>
2012-08-15 18:21:48 +0800

    add handlebarsext module

---
+++ static/js/handlebarsext/0.0.1/handlebarsext.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * A collection of helpers for Handlebars.js
+ */
+define(function (require) {
+  var Handlebars = require('handlebars');
+
+  // String
+  // -----------------------------
+
+  // Return a copy of the string with its first character capitalized and the rest lowercased.
+  Handlebars.registerHelper('capitalize', function (str) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  });
+
+});
