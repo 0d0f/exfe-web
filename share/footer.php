ef5b1ced4e41e738c74f4cd2e8ba33c21e77c5cf
Leask Huang <i@leaskh.com>
2012-08-21 13:50:25 +0800

    upgrade shared footer

--- share/footer.php
+++ share/footer.php
@@ -1,48 +1,64 @@
+<?php
 
-  <script src="/static/js/common/0.0.1/common.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/class/0.0.1/class.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/emitter/0.0.1/emitter.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/base/0.0.1/base.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/bus/0.0.1/bus.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/rex/0.0.1/rex.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/util/0.0.1/util.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/widget/0.0.1/widget.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+function addScript($scripts) {
+    $mini = DEBUG ? '' : '.mini';
+    foreach ($scripts as $item) {
+        echo "  <script src=\"/static/js/{$item[0]}/{$item[1]}/{$item[0]}{$mini}.js?" . STATIC_CODE_TIMESTAMP . "\"></script>\n";
+    }
+}
 
-  <script src="/static/js/jquery/1.7.2/jquery.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/moment/1.6.2/moment.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/store/1.3.3/store.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/marked/0.2.5/marked.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/handlebars/1.0.0/handlebars.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/handlebarsext/0.0.1/handlebarsext.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/jqfocusend/0.0.1/jqfocusend.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/jqoffset/0.0.1/jqoffset.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
 
-  <script>
-  <?php include 'ftconfig.php'; ?>
-  </script>
+echo "\n";
+addScript([
+    ['common',        '0.0.1'],
+    ['class',         '0.0.1'],
+    ['emitter',       '0.0.1'],
+    ['base',          '0.0.1'],
+    ['bus',           '0.0.1'],
+    ['rex',           '0.0.1'],
+    ['util',          '0.0.1'],
+    ['widget',        '0.0.1'],
+    ['jquery',        '1.7.2'],
+    ['moment',        '1.6.2'],
+    ['store',         '1.3.3'],
+    ['marked',        '0.2.5'],
+    ['handlebars',    '1.0.0'],
+    ['handlebarsext', '0.0.1'],
+    ['jqfocusend',    '0.0.1'],
+    ['jqoffset',      '0.0.1'],
+]);
 
-  <script src="/static/js/global/0.0.1/global.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+echo "  <script>\n";
+include 'ftconfig.php';
+echo "  </script>\n";
 
-  <script src="/static/js/api/0.0.1/api.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/dialog/0.0.1/dialog.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/typeahead/0.0.1/typeahead.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/xidentity/0.0.1/xidentity.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/xdialog/0.0.1/xdialog.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+addScript([
+    ['global',        '0.0.1'],
+    ['api',           '0.0.1'],
+    ['dialog',        '0.0.1'],
+    ['typeahead',     '0.0.1'],
+    ['xidentity',     '0.0.1'],
+    ['xdialog',       '0.0.1'],
+]);
 
-  <!-- profile -->
-  <script src="/static/js/filehtml5/0.0.1/filehtml5.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/uploader/0.0.1/uploader.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/profile/0.0.1/profile.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+// profile
+addScript([
+    ['filehtml5',     '0.0.1'],
+    ['uploader',      '0.0.1'],
+    ['profile',       '0.0.1'],
+]);
 
-  <!-- cross -->
-  <script src="/static/js/placepanel/0.0.1/placepanel.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/_cross.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+// cross
+addScript([
+    ['placepanel',    '0.0.1'],
+    ['user',          '0.0.1'],
+]);
+echo "  <script src=\"/static/_cross.js?" . STATIC_CODE_TIMESTAMP . "\"></script>\n";
 
-  <script src="/static/js/user/0.0.1/user.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-
-  <!-- lightsaber -->
-  <script src="/static/js/lightsaber/0.0.1/lightsaber.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-
-  <script src="/static/js/middleware/0.0.1/middleware.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/routes/0.0.1/routes.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
-  <script src="/static/js/app/0.0.1/app.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
+// lightsaber
+addScript([
+    ['lightsaber',    '0.0.1'],
+    ['middleware',    '0.0.1'],
+    ['routes',        '0.0.1'],
+    ['app',           '0.0.1'],
+]);
