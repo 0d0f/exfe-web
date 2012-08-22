eaa2508c2fb0b66d65f71160c468b7462f2e1d9a
cfddream <cfddream@gmail.com>
2012-08-22 13:35:28 +0800

    update favicon

--- share/header.php
+++ share/header.php
@@ -16,7 +16,7 @@
 <?php if (($twitterSignin = $this->getVar('twitter_signin'))) { ?>
   <meta name="authorization" content="<?php echo htmlentities(json_encode($twitterSignin)); ?>" />
 <?php } ?>
-  <link rel="shortcut icon" href="/static/img/favicon_16.png" />
-  <link rel="apple-touch-icon" href="/static/img/favicon_32.png" />
+  <link rel="shortcut icon" href="/static/img/favicon.png" />
+  <link rel="apple-touch-icon" href="/static/img/favicon.png" />
   <link rel="stylesheet" media="screen" type="text/css" href="/static/css/exfe.min.css?<?php echo STATIC_CODE_TIMESTAMP; ?>" />
   <script src="/static/js/modernizr/2.5.3/modernizr.min.js?<?php echo STATIC_CODE_TIMESTAMP; ?>"></script>
