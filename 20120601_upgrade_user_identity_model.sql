c2f94ea807e3d905e029345083c1e7fe7d6bc113
Leask Huang <i@leaskh.com>
2012-08-23 14:33:44 +0800

    upgrade get cross by token and adding a script to upgrade cross time from old data

--- 20120601_upgrade_user_identity_model.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE `users` DROP COLUMN `avatar_content_type`;
-ALTER TABLE `users` DROP COLUMN `avatar_file_size`;
-ALTER TABLE `users` DROP COLUMN `avatar_updated_at`;
-ALTER TABLE `users` DROP COLUMN `external_username`;
-
-ALTER TABLE `identities` DROP COLUMN `avatar_content_type`;
-ALTER TABLE `identities` DROP COLUMN `avatar_file_size`;
-ALTER TABLE `identities` DROP COLUMN `avatar_updated_at`;
