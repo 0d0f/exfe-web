79e1b0d609e127d220b5cbdb3e6e7c2ef6168a12
Leask Huang <i@leaskh.com>
2012-08-23 18:45:19 +0800

    fixed a bug in cross time render

--- static/_cross.js
+++ static/_cross.js
@@ -56,7 +56,8 @@ ExfeUtilities = {
                 begin_at : {
                     date_word : '', date : '',
                     time_word : '', time : '',
-                    timezone  : this.getTimezone(), id : 0, type : 'EFTime'
+                    timezone  : Cross.id ? Cross.time.begin_at.timezone : this.getTimezone(),
+                    id : 0, type : 'EFTime'
                 },
                 origin : strTime, outputformat : 1, id : 0, type : 'CrossTime'
             },
