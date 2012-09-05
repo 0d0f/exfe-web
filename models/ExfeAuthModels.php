ff06884ddccfd54953a1a0568a08372c7c5b6f16
Leask Huang <i@leaskh.com>
2012-09-05 18:40:06 +0800

    finished merge gobus exfeauth

--- models/ExfeAuthModels.php
+++ models/ExfeAuthModels.php
@@ -4,6 +4,13 @@ class ExfeAuthModels extends DataModel {
 
     protected function useTokenApi($method, $args) {
         if ($method && $args) {
+            if (is_array($args)) {
+                foreach ($args as $aI => $aItem) {
+                    if (is_array($aItem)) {
+                        $args[$aI] = json_encode($aItem);
+                    }
+                }
+            }
             $url      = EXFE_AUTH_SERVER . "/TokenManager?method={$method}";
             $objCurl  = curl_init();
             curl_setopt($objCurl, CURLOPT_URL, $url);
@@ -12,12 +19,20 @@ class ExfeAuthModels extends DataModel {
             curl_setopt($objCurl, CURLOPT_CONNECTTIMEOUT, 3);
             curl_setopt($objCurl, CURLOPT_POST, 1);
             curl_setopt($objCurl, CURLOPT_POSTFIELDS, json_encode($args));
-            $httpBody = @json_decode(curl_exec($objCurl));
-            print_r($httpBody);
+            $httpBody = @json_decode(curl_exec($objCurl), true);
             $httpCode = curl_getinfo($objCurl, CURLINFO_HTTP_CODE);
             curl_close($objCurl);
             if ($httpCode === 200 && $httpBody) {
+                if (is_array($httpBody)) {
+                    foreach ($httpBody as $hI => $hItem) {
+                        $httpBody[$hI] = (
+                            $rtDecode = @json_decode($hItem, true)
+                        ) === null ? $hItem : $rtDecode;
+                    }
+                }
                 return $httpBody;
+            } else if (DEBUG) {
+                error_log($httpBody);
             }
         }
         return null;
