4de0b0b2d6ead43c37950d8f90d42ef30fffb337
Leask Huang <i@leaskh.com>
2012-09-10 16:45:40 +0800

    reactived token service

--- controllers/api/v2/UsersActions.php
+++ controllers/api/v2/UsersActions.php
@@ -434,24 +434,13 @@ class UsersActions extends ActionController {
             // try connected user
             if (!isset($user_infos['CONNECTED'])) {
                 // clear verify token
-                // if (isset($user_infos['VERIFYING'])) {
-                //     $modExfeAuth->expireAllTokens([
-                //         'token_type'   => 'verification_token',
-                //         'action'       => 'VERIFY',
-                //         'identity_id'  => $invitation['identity_id'],
-                //     ]);
-                // }
-
-                ////////////////////////////////////////////////////////////////
-                // @todo upgraded by @leaskh
-                // clear verify token
                 if (isset($user_infos['VERIFYING'])) {
-                    $modUser->destroySimilarTokens(
-                        $invitation['identity_id'], 'VERIFY'
-                    );
+                    $modExfeAuth->expireAllTokens([
+                        'token_type'   => 'verification_token',
+                        'action'       => 'VERIFY',
+                        'identity_id'  => $invitation['identity_id'],
+                    ]);
                 }
-                ////////////////////////////////////////////////////////////////
-
                 // add new user
                 $user_id = $modUser->addUser($passwd, $name);
                 // connect identity to new user
