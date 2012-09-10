edd474905997d4e9ed89d2f6e7c4ae7af3d5615a
Leask Huang <i@leaskh.com>
2012-09-10 15:54:20 +0800

    fixed a bug in default avatar getting

--- static/_cross.js
+++ static/_cross.js
@@ -580,7 +580,8 @@ ExfeeWidget = {
         } else {
             return null;
         }
-        objIdentity.avatar_filename = ExfeeWidget.api_url + '/avatar/get?default=' + objIdentity.name;
+        objIdentity.avatar_filename = ExfeeWidget.api_url + '/avatar/default?name=' + objIdentity.name;
+        console.log(objIdentity.avatar_filename);
         if (objIdentity.provider === 'email') {
             objIdentity.avatar_filename = 'http://www.gravatar.com/avatar/'
                                         + MD5(objIdentity.external_username)
