9c402ade80bc2adcf6495821fc609b79ac759475
Leask Huang <i@leaskh.com>
2012-09-20 01:54:12 +0800

    fixed a bug in ExfeeModels.php editing feature

--- models/ExfeeModels.php
+++ models/ExfeeModels.php
@@ -421,9 +421,9 @@ class ExfeeModels extends DataModel {
                         } else {
                             $updateToken = false;
                         }
-                        if ($fmItem->rsvp_status !== $toItem->rsvp_status
-                         || $fmItem->host        !== $toItem->host
-                         || $fmItem->mates       !== $toItem->mates) {
+                        if ($fmItem->rsvp_status  !== $toItem->rsvp_status
+                         || (bool) $fmItem->host  !== (bool) $toItem->host
+                         || (int)  $fmItem->mates !== (int)  $toItem->mates) {
                             $this->updateInvitation($toItem, $by_identity_id, $updateToken);
                             $changed = true;
                         }
