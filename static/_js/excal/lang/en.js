a59bc91ff5947ea049255d58728376c4666e06ec
Leask Huang <i@leaskh.com>
2012-09-20 22:06:58 +0800

    removed old version of excal

--- static/_js/excal/lang/en.js
+++
@@ -1,9 +0,0 @@
-var exLang={
-    weekArr: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-    longWeekArr:["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"],
-    monthArr: ["January","February","March","April","May","June","July","August","September","October","November","December"],
-    todayBtnStr: "Today",
-    timeBtnStr: "Time",
-    timeAllDay: "All-day",
-    errorMsg: []
-};
