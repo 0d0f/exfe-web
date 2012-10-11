13a48debc813808c7ac15dedef492379edf08026
cfddream <cfddream@gmail.com>
2012-10-11 15:06:43 +0800

    removed comments (cherry picked from commit 8c40b3afcf12a447a80cdf935875373a479d0470)

--- share/footer.php
+++ share/footer.php
@@ -39,7 +39,7 @@ addScript([
     ['dialog',        '0.0.3'],
     ['typeahead',     '0.0.2'],
     ['xidentity',     '0.0.1'],
-    ['xdialog',       '0.0.3'],
+    ['xdialog',       '0.0.4'],
     ['global',        '0.0.5'],
 ]);
 
