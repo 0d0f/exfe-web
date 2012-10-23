c3817c6043c54da65eab4fcf28dfe3a33cea9487
Leask Huang <i@leaskh.com>
2012-10-23 12:13:59 +0800

    working on upgrading user model

--- Classes/User.php
+++ Classes/User.php
@@ -14,14 +14,28 @@ class User extends EFObject {
 
     public $identities        = null;
 
+    public $devices           = null;
+
+    public $created_at        = null;
+
+    public $updated_at        = null;
+
     public function __construct($id                = 0,
                                 $name              = '',
                                 $bio               = '',
                                 $default_identity  = null,
                                 $avatar_filename   = '',
                                 $timezone          = '',
-                                $identities        = array()) {
+                                $identities        = [],
+                                $devices           = [],
+                                $created_at        = '',
+                                $updated_at        = '') {
         parent::__construct($id, 'user');
+
+        $created_at              = $created_at ?:  '0000-00-00 00:00:00';
+        $updated_at              = $updated_at
+                                && $updated_at !== '0000-00-00 00:00:00'
+                                 ? $updated_at : $created_at;
 
         $this->name              = $name;
         $this->bio               = $bio ?: '';
@@ -29,6 +43,10 @@ class User extends EFObject {
         $this->avatar_filename   = $avatar_filename;
         $this->timezone          = $timezone ?: '';
         $this->identities        = $identities;
+        $this->devices           = $devices;
+        $this->created_at        = $created_at . ' +0000';
+        $this->updated_at        = $updated_at . ' +0000';
+
     }
 
 }
