3f338a1d97202f870a64376520fd053279a9fd1b
Leask Huang <i@leaskh.com>
2012-10-31 22:11:46 +0800

    extending gobus model and helpers

--- models/ExfeAuthModels.php
+++ models/ExfeAuthModels.php
@@ -2,40 +2,18 @@
 
 class ExfeAuthModels extends DataModel {
 
+    public $hlpGobus = null;
+
+
+    public function __construct() {
+        $this->hlpGobus = $this->getHelperByName('Gobus');
+    }
+
+
     protected function useTokenApi($method, $args) {
-        if ($method && $args) {
-            if (is_array($args)) {
-                foreach ($args as $aI => $aItem) {
-                    if (is_array($aItem)) {
-                        $args[$aI] = json_encode($aItem);
-                    }
-                }
-            }
-            $url      = EXFE_AUTH_SERVER . "/TokenManager?method={$method}";
-            $objCurl  = curl_init();
-            curl_setopt($objCurl, CURLOPT_URL, $url);
-            curl_setopt($objCurl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($objCurl, CURLOPT_HEADER, false);
-            curl_setopt($objCurl, CURLOPT_CONNECTTIMEOUT, 3);
-            curl_setopt($objCurl, CURLOPT_POST, 1);
-            curl_setopt($objCurl, CURLOPT_POSTFIELDS, json_encode($args));
-            $httpBody = @json_decode(curl_exec($objCurl), true);
-            $httpCode = curl_getinfo($objCurl, CURLINFO_HTTP_CODE);
-            curl_close($objCurl);
-            if ($httpCode === 200 && $httpBody) {
-                if (is_array($httpBody)) {
-                    foreach ($httpBody as $hI => $hItem) {
-                        $httpBody[$hI] = (
-                            $rtDecode = @json_decode($hItem, true)
-                        ) === null ? $hItem : $rtDecode;
-                    }
-                }
-                return $httpBody;
-            } else if (DEBUG) {
-                error_log($httpBody);
-            }
-        }
-        return null;
+        return $this->hlpGobus->useGobusApi(
+            EXFE_AUTH_SERVER, 'TokenManager', $method, $args
+        );
     }
 
 
