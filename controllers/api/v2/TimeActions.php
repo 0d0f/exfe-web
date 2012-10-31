5f51fda90eae0784ad35d675ac3cf0bc5a6d28ad
Leask Huang <i@leaskh.com>
2012-10-31 16:37:39 +0800

    finished time recognize api

--- controllers/api/v2/TimeActions.php
+++ controllers/api/v2/TimeActions.php
@@ -11,9 +11,13 @@ class TimeActions extends ActionController {
         if (!($strTime = @trim($_POST['time_string']))) {
             apiError(400, 'no_time_string', '');
         }
+        if (!($strZone = @trim($_POST['timezone']))) {
+            apiError(400, 'no_timezone', '');
+        }
         $modTime = $this->getModelByName('Time');
-        $modTime->parseTimeString($strTime);
-
+        apiResponse([
+            'cross_time' => $modTime->parseTimeString($strTime, $strZone),
+        ]);
     }
 
 }
