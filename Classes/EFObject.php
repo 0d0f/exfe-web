926b8ca20e8c6a835cde14eb7286e47b58c85ce8
Leask Huang <i@leaskh.com>
2012-11-01 16:12:06 +0800

    finished porting conversation to the new queue

--- Classes/EFObject.php
+++ Classes/EFObject.php
@@ -6,13 +6,14 @@ require_once 'User.php';
 require_once 'Identity.php';
 require_once 'Exfee.php';
 require_once 'Invitation.php';
+require_once 'Recipient.php';
 
 abstract class EFobject{
-    
+
     public $id   = null;
 
     public $type = null;
-    
+
     public function __construct($id = 0, $type = 'EFObject') {
         $this->id   = intval($id);
         $this->type = $type;
