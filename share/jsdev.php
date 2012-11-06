6db1bf771b50c150d59e2a7f31ab9b105170159e
cfddream <cfddream@gmail.com>
2012-11-06 12:16:05 +0800

    fixed (cherry picked from commit 1947fa7edfd7cf0ba9afb9f6ff74726005470273)
    Conflicts:
    	frontend/jslib/util/lib/util.js
    	static/js/util/0.0.3/util.js
    	static/js/util/0.0.3/util.min.js

--- share/jsdev.php
+++ share/jsdev.php
@@ -9,11 +9,7 @@ $jsdev = <<<EOD
   e.style.borderTop = '6px solid #D32232';
   e.style.borderBottom = '5px solid {{JS_COLOR}}';
   document.body.appendChild(e);
-<<<<<<< HEAD
-</script>s
-=======
 </script>
->>>>>>> master
 EOD;
 
 echo str_replace('{{JS_COLOR}}', JS_COLOR, $jsdev);
