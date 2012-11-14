fe2ee1cf88baa6bf3d670f2c3a20ac2000b21095
Leask Huang <i@leaskh.com>
2012-11-14 20:46:16 +0800

    adding doNotificationCallback api for gobus

--- 20121113_upgraded_invitation_and_identity_schema.sql
+++ 20121113_upgraded_invitation_and_identity_schema.sql
@@ -1,3 +1,4 @@
 ALTER TABLE `invitations` ADD COLUMN `invited_by`  bigint(20) unsigned;
-ALTER TABLE `identities`  ADD COLUMN `unreachable` tinyint(1);
+ALTER TABLE `identities`  ADD COLUMN `unreachable` tinyint(1) default 0;
+ALTER TABLE `devices`     ADD COLUMN `unreachable` tinyint(1) default 0;
 UPDATE `invitations` SET `invited_by`=`by_identity_id`;
