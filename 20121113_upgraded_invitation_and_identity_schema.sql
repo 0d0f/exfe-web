307bee67a02d548d08a01750a83cee162f75774f
Leask Huang <i@leaskh.com>
2012-11-15 16:13:46 +0800

    adding geoip city data into our project

--- 20121113_upgraded_invitation_and_identity_schema.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `invitations` ADD COLUMN `invited_by`  bigint(20) unsigned;
-ALTER TABLE `identities`  ADD COLUMN `unreachable` tinyint(1) default 0;
-ALTER TABLE `devices`     ADD COLUMN `unreachable` tinyint(1) default 0;
-UPDATE `invitations` SET `invited_by`=`by_identity_id`;
