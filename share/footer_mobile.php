4f912cac4e6e0ab56b66f41a0172471a98089b77
Leask Huang <i@leaskh.com>
2012-12-03 14:59:55 +0800

    adding mobile view

---
+++ share/footer_mobile.php
@@ -0,0 +1,37 @@
+<?php
+
+function addScript($scripts) {
+    $min = JS_DEBUG ? '' : '.min';
+    foreach ($scripts as $item) {
+        echo "  <script src=\"/static/js/{$item[0]}/{$item[1]}/{$item[0]}{$min}.js?" . STATIC_CODE_TIMESTAMP . "\"></script>\n";
+    }
+}
+
+
+echo "\n";
+addScript([
+    ['common',        '0.0.3'],
+    ['zepto',         '1.0.0'],
+    ['mobile',        '0.0.1'],
+]);
+
+
+// Google Analytics
+if (SITE_URL === 'https://exfe.com') {
+echo <<<EOT
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-31794223-2']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+EOT;
+}
+
+if (SITE_URL !== 'https://exfe.com') {
+    require 'jsdev.php';
+}
