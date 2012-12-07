11a217edf5e8900ff1b9dcce336446dfa1d33b89
Leask Huang <i@leaskh.com>
2012-12-07 11:16:22 +0800

    adding simple view to mobile version

--- views/home/mobileView.php
+++ views/home/mobileView.php
@@ -1,8 +1,21 @@
+<?php include "share/header_mobile.php" ?>
 </head>
 <body>
    <!-- Container {{{-->
   <div class="container" id="app-container">
-    <div role="main" id="app-main"></div>
+    <div role="main" id="app-main">
+        <div class="top-banner">Welcome to EXFE!</div>
+        <div class="dialog-box">
+            <div class="input-area">
+                <img class="avatar" src=""/>
+                <span class="provider"></span>
+                <input type="text"/>
+                <button>X</button>
+            </div>
+            <div class="input-area"></div>
+            <button>Start</button>
+        </div>
+    </div>
   </div>
   <!--/Container }}}-->
 
