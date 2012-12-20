5b4c2da1a1c225d983ea349b98cd076fedb36407
Leask Huang <i@leaskh.com>
2012-12-21 00:09:42 +0800

    esolving the conflicts in IdentityModels

--- models/ExfeeModels.php
+++ models/ExfeeModels.php
@@ -476,8 +476,9 @@ class ExfeeModels extends DataModel {
 
 
     public function updateExfeeTime($exfee_id) {
-        $sql="update invitations set exfee_updated_at=NOW() where `cross_id`=$exfee_id;";
+        $sql="update invitations set exfee_updated_at=NOW() where `cross_id`={$exfee_id};";
         $this->query($sql);
+        delCache("exfee:{$exfee_id}");
     }
 
 
