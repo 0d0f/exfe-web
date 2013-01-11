c1108ff7e7d35430f6760ea02fc41da42a65ea0e
Leask Huang <i@leaskh.com>
2013-01-11 14:20:52 +0800

    finished exfe ChangeIdentity api

--- helpers/CheckHelper.php
+++ helpers/CheckHelper.php
@@ -58,32 +58,34 @@ class CheckHelper extends ActionController {
         switch ($api) {
             case 'cross':
             case 'cross_edit':
-                $exfee_id=$crossData->getExfeeByCrossId($args["cross_id"]);
-                $userids=$exfeeData->getUserIdsByExfeeId($exfee_id, true);
+                $exfee_id = $crossData->getExfeeByCrossId($args["cross_id"]);
+                $userids = $exfeeData->getUserIdsByExfeeId($exfee_id, true);
                 if (in_array($uid, $userids)) {
-                    if ($api=="cross_edit") {
-                        $by_identity_id=$args["by_identity_id"];
+                    if ($api == "cross_edit") {
+                        $by_identity_id = $args["by_identity_id"];
                         if (intval($by_identity_id)>0) {
-                            $r=$identityData->isIdentityBelongsUser($by_identity_id,$uid);
-                            if ($r===true) {
-                                return array("check"=>true,"uid"=>$uid,"exfee_id"=>$exfee_id,"by_identity_id"=>$by_identity_id);
+                            // @todo maybe 安全漏洞！ by @leaskh
+                            $r = $identityData->isIdentityBelongsUser($by_identity_id, $uid, false);
+                            if ($r === true) {
+                                return array("check" => true, "uid"=>$uid, "exfee_id" => $exfee_id, "by_identity_id" => $by_identity_id);
                             } else {
-                                return array("check"=>false);
+                                return array("check" => false);
                             }
                         } else {
-                            return array("check"=>false);
+                            return array("check" => false);
                         }
                     }
-                    return array("check"=>true,"uid"=>$uid,"exfee_id"=>$exfee_id);
+                    return array("check" => true, "uid" => $uid, "exfee_id" => $exfee_id);
                 }
                 break;
             case 'cross_add':
-                $by_identity_id=$args["by_identity_id"];
-                $r=$identityData->isIdentityBelongsUser($by_identity_id,$uid);
-                if ($r===true) {
-                    return array("check"=>true,"uid"=>$uid,"by_identity_id"=>$by_identity_id);
+                $by_identity_id = $args["by_identity_id"];
+                // @todo maybe 安全漏洞！ by @leaskh
+                $r = $identityData->isIdentityBelongsUser($by_identity_id, $uid, false);
+                if ($r === true) {
+                    return array("check" => true, "uid" => $uid, "by_identity_id" => $by_identity_id);
                 } else {
-                    return array("check"=>false);
+                    return array("check" => false);
                 }
                 break;
             case 'conversation':
@@ -97,12 +99,12 @@ class CheckHelper extends ActionController {
                 $idntIds = $exfeeData->getIdentityIdsByExfeeId($args['exfee_id']);
                 if (in_array($uid, $userids)
                  && in_array($args['identity_id'], $idntIds)) {
-                    return array('check' => true, 'uid'=>$uid, 'by_identity_id'=>$args['identity_id']);
+                    return array('check' => true, 'uid' => $uid, 'by_identity_id' => $args['identity_id']);
                 }
                 break;
             case 'conversation_del':
-                if ($uid==$args["user_id"]) {
-                    return array("check"=>true,"uid"=>$uid);
+                if ($uid == $args["user_id"]) {
+                    return array("check" => true, "uid" => $uid);
                 }
                 break;
             case 'user_self':
