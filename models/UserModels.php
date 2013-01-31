8c9b16bc83c4fe14eae7cee6d433625c8f492b06
Leask Huang <i@leaskh.com>
2013-01-31 11:36:10 +0800

    finished flickr api

--- models/UserModels.php
+++ models/UserModels.php
@@ -510,6 +510,7 @@ class UserModels extends DataModel {
             case 'twitter':
             case 'facebook':
             case 'dropbox':
+            case 'flickr':
             case 'instagram':
                 $hlpOAuth = $this->getHelperByName('OAuth');
                 $workflow = ['user_id' => $user_id];
@@ -547,6 +548,9 @@ class UserModels extends DataModel {
                         break;
                     case 'dropbox':
                         $urlOauth = $hlpOAuth->dropboxRedirect($workflow);
+                        break;
+                    case 'flickr':
+                        $urlOauth = $hlpOAuth->flickrRedirect($workflow);
                         break;
                     case 'instagram':
                         $urlOauth = $hlpOAuth->instagramRedirect($workflow);
