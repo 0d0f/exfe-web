04871fe0041531f3009161cc5ba7efa8ed95ca40
Leask Huang <i@leaskh.com>
2013-02-05 15:37:10 +0800

    adding response model

---
+++ models/Response.php
@@ -0,0 +1,19 @@
+<?php
+
+class ResponseModels extends DataModel {
+
+    public function getResponseOfObject($object_type, $object_ids) {
+
+    }
+
+
+    public function responseToObject($object_type, $object_id, $response) {
+
+    }
+
+
+    public function () {
+
+    }
+
+}
