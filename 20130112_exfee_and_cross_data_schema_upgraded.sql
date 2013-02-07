0e1aac497ae2603fa279d1db0fc5988335fa37b7
Leask Huang <i@leaskh.com>
2013-02-07 12:37:51 +0800

    removed old upgraded scripts

--- 20130112_exfee_and_cross_data_schema_upgraded.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `exfees`      ADD COLUMN `name`   text;
-
-ALTER TABLE `crosses`     ADD COLUMN `closed` tinyint(1) DEFAULT 0;
-
-ALTER TABLE `invitations` ADD COLUMN `remark` varchar(255);
