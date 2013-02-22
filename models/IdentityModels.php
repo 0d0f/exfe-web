fc6aeaa81fec5e8b1488d345bc251754dfb53d9d
Leask Huang <i@leaskh.com>
2013-02-22 13:02:42 +0800

    adding validatePhoneNumber

--- models/IdentityModels.php
+++ models/IdentityModels.php
@@ -275,8 +275,15 @@ class IdentityModels extends DataModel {
         // basic check
         switch ($provider) {
             case 'email':
+                if (!$external_id && !$external_username) {
+                    return null;
+                }
+                break;
             case 'phone':
                 if (!$external_id && !$external_username) {
+                    return null;
+                }
+                if (!validatePhoneNumber($external_id ?: $external_username)) {
                     return null;
                 }
                 break;
