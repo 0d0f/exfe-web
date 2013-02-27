a4efd98fcb7f795cd593d16a0af711d2e576e814
cfddream <cfddream@gmail.com>
2013-02-27 15:50:38 +0800

    Upgraded `humantime.js`-v0.0.6

--- share/footer.php
+++ share/footer.php
@@ -35,7 +35,7 @@ include 'ftconfig.php';
 echo "  </script>\n";
 
 addScript([
-    ['humantime',     '0.0.5'],
+    ['humantime',     '0.0.6'],
     ['api',           '0.0.8'],
     ['dialog',        '0.0.4'],
     ['typeahead',     '0.0.2'],
