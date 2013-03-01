0f5dbbf8f3d35fee063ffab7b957fbb13eacc9ac
Leask Huang <i@leaskh.com>
2013-03-01 18:25:35 +0800

    adding apple-mobile-web-app-status-bar-style black to iphone

--- share/header_mobile.php
+++ share/header_mobile.php
@@ -14,6 +14,7 @@
   <meta name="copyright" content="Copyright &copy; 2012 EXFE Inc" />
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
   <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="black" />
 <?php if (($oauth = $this->getVar('oauth'))) { ?>
   <meta name="authorization" content="<?php echo htmlentities(json_encode($oauth)); ?>" />
 <?php } ?>
