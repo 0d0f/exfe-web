8a46290cd0ad9e15224faa486d5e40747aaab27a
Leask Huang <i@leaskh.com>
2013-04-10 15:45:33 +0800

    adding sort arg in get photox by id api

--- controllers/api/v2/PhotoxActions.php
+++ controllers/api/v2/PhotoxActions.php
@@ -19,7 +19,7 @@ class PhotoxActions extends ActionController {
                 apiError(403, 'not_authorized', "The PhotoX you're requesting is private.");
             }
             $modPhotos = $this->getModelByName('Photo');
-            $photox    = $modPhotos->getPhotoxById($params['id']);
+            $photox    = $modPhotos->getPhotoxById($params['id'], isset($params['sort']) ? $params['sort'] : '');
             $responses = $modPhotos->getResponsesByPhotoxId($params['id']);
             apiResponse(['photox' => $photox, 'likes' => $responses]);
         }
