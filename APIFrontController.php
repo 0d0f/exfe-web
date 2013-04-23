1b6f0a082108d5f3331eb69d237e6c3c1d9344f8
Leask Huang <i@leaskh.com>
2013-04-23 14:28:40 +0800

    tidy codes and removed qieke codes

--- APIFrontController.php
+++ APIFrontController.php
@@ -1,6 +1,6 @@
 <?php
-require_once dirname(__FILE__)."/ActionController.php";
-require_once dirname(__FILE__)."/DataModel.php";
+require_once dirname(__FILE__) . '/ActionController.php';
+require_once dirname(__FILE__) . '/DataModel.php';
 
 class FrontController {
 
@@ -15,54 +15,6 @@ class FrontController {
             }
         }
     }
-
-#	public static function checkOauthToken() {
-#
-#	if(isset($_GET["magic_token"])) {
-#
-#		$_SESSION["userid"] = $_GET["magic_token"];
-#
-#		return;
-#
-#	} else {
-#
-#		$accesstoken = '';
-#
-#		if(isset($_GET['oauth_token']) ) {
-#			$accesstoken=$_GET['oauth_token'];
-#		}
-#		else if(isset($_GET['access_token']) ) {
-#			$accesstoken=$_GET['access_token'];
-#		}
-#
-#		if ($accesstoken=="") {
-#
-#			$responobj["meta"]["code"]=400;
-#			$responobj["meta"]["errType"]="Bad Request";
-#			$responobj["meta"]["errorDetail"]="invalid_auth";
-#			echo json_encode($responobj);
-#			exit();
-#
-#		} else {
-#
-#			$name="user";
-#	    		$class = ucfirst($name) . "Models";
-#	    		$modelfile = MODEL_DIR . "/" . $name. "/" . $class.  ".php";
-#	    		include_once $modelfile;
-#	    		$userData=new $class;
-#			$useruuid=$userData->getUserIdByAccessToken($accesstoken);
-#			if(intval($useruuid)>0) {
-#				$_SESSION["userid"]=$useruuid;
-#			} else {
-#				$responobj["meta"]["code"]=401;
-#				$responobj["meta"]["errType"]="invalid_auth";
-#				$responobj["meta"]["errorDetail"]="invalid_auth";
-#				echo json_encode($responobj);
-#				exit();
-#			 }
-#		}
-#	}
-#	}
 
     public static function createInstance() {
         if (!defined("PAGE_DIR")) {
@@ -103,11 +55,11 @@ class FrontController {
 
         foreach($_SERVER as $name=>$value)
         {
-            if (substr($name, 0, 5) == 'HTTP_') 
-           { 
-               $name = str_replace(' ', '-', strtolower(str_replace('_', ' ', substr($name, 5)))); 
+            if (substr($name, 0, 5) == 'HTTP_')
+           {
+               $name = str_replace(' ', '-', strtolower(str_replace('_', ' ', substr($name, 5))));
                $params[$name]=$value;
-           } 
+           }
         }
 
         require_once $file;
--- ActionController.php
+++ ActionController.php
@@ -7,23 +7,8 @@ abstract class ActionController {
     protected $name;
     protected $model;
     protected $action;
-    protected $viewData = array();
-    protected $modelArray=array();
-
-    //public function genUUID($type="101") {
-    //	return file_get_contents("http://api.qieke.local:8080/u?$type");
-    //}
-
-    //  public function getModelByName($modelName) {
-    //
-    //        if(array_key_exists($modelName, $this->modelArray)) {
-    //                return $this->modelArray[$modelName];
-    //        } else {
-    //                $model = $this->_getModelByName($modelName);
-    //                $this->modelArray[$modelName] = $model;
-    //                return $model;
-    //        }
-    //}
+    protected $viewData   = [];
+    protected $modelArray = [];
 
     // public function __construct() {
     //     ini_set('session.cookie_domain', ROOT_DOMAIN);
@@ -32,122 +17,111 @@ abstract class ActionController {
     public function setName($name) {
         $this->name = $name;
     }
+
     public function getName() {
         return $this->name;
     }
+
     public function getModel() {
         return $this->model;
     }
+
     public function setVar($key, $value) {
         $this->viewData[$key] = $value;
     }
+
     public function getVar($key) {
         if (array_key_exists($key, $this->viewData)) {
             return $this->viewData[$key];
         }
     }
 
-    public function displayViewByNameAction($name,$action) {
-        if (!is_file(VIEW_DIR . "/" . $name . "/" . $action . "View.php")) {
-            exit("Page not found:".$name."::". $action . "View.php");
+    public function displayViewByNameAction($name, $action) {
+        if (!is_file(VIEW_DIR . '/' . $name . '/' . $action . 'View.php')) {
+            exit('Page not found:' . $name . '::' . $action . 'View.php');
         }
-        //Create variables for the template
+        // Create variables for the template
         foreach ($this->viewData as $key => $value) {
             $key = $value;
         }
-        include VIEW_DIR . "/" . $name . "/" . $action . "View.php";
+        include VIEW_DIR . '/' . $name . '/' . $action . 'View.php';
         exit(0);
     }
 
     public function displayViewByAction($action) {
         $this->displayViewByNameAction($this->getName(),$action);
-#  if (!is_file(VIEW_DIR . "/" . $action . "View.php")) {
-#    exit("Page not found:" . $action . "View.php");
-#  }
-#  //Create variables for the template
-#  foreach ($this->viewData as $key => $value) {
-#    $key = $value;
-#  }
-#  include VIEW_DIR . "/" . $action . "View.php";
-#  exit(0);
     }
 
-    public function displayView()
-    {
+    public function displayView() {
         $this->displayViewByAction($this->action);
     }
 
-    public function getHelperByNamev1($name)
-    {
-        $class = ucfirst($name) . "Helper";
-        $helperfile = HELPER_DIR. "/" . $class.  ".php";
+    public function getHelperByNamev1($name) {
+        $class = ucfirst($name) . 'Helper';
+        $helperfile = HELPER_DIR. '/' . $class . '.php';
         include_once $helperfile;
         return new $class;
     }
-    public function getHelperByName($name,$version="")
-    {
-        if($version=="")
+
+    public function getHelperByName($name, $version = '') {
+        if($version == '') {
             $this->getHelperByNamev1($name);
+        }
 
-        $class = ucfirst($name) . "Helper";
-        $helperfile = HELPER_DIR. "/".$version. "/" . $class.  ".php";
+        $class = ucfirst($name) . 'Helper';
+        $helperfile = HELPER_DIR . '/' . $version. '/' . $class . '.php';
         include_once $helperfile;
         return new $class;
     }
 
-
-    public function getModelByNamev1($name)
-    {
-        $class = ucfirst($name) . "Models";
-        $modelfile = MODEL_DIR . "/"  . $class.  ".php";
+    public function getModelByNamev1($name) {
+        $class = ucfirst($name) . 'Models';
+        $modelfile = MODEL_DIR . '/' . $class . '.php';
         include_once $modelfile;
         return new $class;
     }
-    public function getModelByName($name,$version="")
-    {
-        if($version=="")
+
+    public function getModelByName($name, $version = '') {
+        if($version == '') {
             $this->getModelByNamev1($name);
-
-        $class = ucfirst($name) . "Models";
-        $modelfile = MODEL_DIR . "/".$version. "/" . $class.  ".php";
+        }
+        $class = ucfirst($name) . 'Models';
+        $modelfile = MODEL_DIR . '/' . $version . '/' . $class . '.php';
         include_once $modelfile;
         return new $class;
     }
 
-
-    public function getDataModel($action)
-    {
-        $class = ucfirst($this->getName()) . "Models";
-        $modelfile = MODEL_DIR . "/" .  $class.  ".php";
+    public function getDataModel($action) {
+        $class = ucfirst($this->getName()) . 'Models';
+        $modelfile = MODEL_DIR . '/' .  $class . '.php';
         include_once $modelfile;
-        $this->model=new $class;
+        $this->model = new $class;
         return $this->model;
     }
 
-    public function dispatchAPIAction($action,$params)
-    {
-        $actionMethod = "do" . ucfirst($action);
+    public function dispatchAPIAction($action, $params) {
+        $actionMethod = 'do' . ucfirst($action);
         if (!method_exists($this, $actionMethod)) {
             header('HTTP/1.1 404 Not Found');
-            exit("Action not found:".$actionMethod);
+            exit('Action not found:' . $actionMethod);
         }
-        $this->action=$action;
-        $this->params=$params;
+        $this->action = $action;
+        $this->params = $params;
         $this->$actionMethod();
     }
+
     public function dispatchAction($action) {
-        $actionMethod = "do" . ucfirst($action);
+        $actionMethod = 'do' . ucfirst($action);
         if (!method_exists($this, $actionMethod)) {
             header('HTTP/1.1 404 Not Found');
-            exit("Action not found:".$actionMethod);
+            exit('Action not found:' . $actionMethod);
         }
         $this->action=$action;
-#$this->getDataModel($action);
         $this->$actionMethod();
-#$this->displayView($action);
     }
 
     public function getPhotoArray($arrays) {
-        return array("count"=>count($arrays), 'items'=>empty($arrays)?array():$arrays);
+        return ['count' => count($arrays), 'items' => empty($arrays) ? [] : $arrays];
     }
+
 }
