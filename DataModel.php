e11822abf7adaccf82c80a2abd3f2cefe0206bb0
Leask Huang <i@leaskh.com>
2013-04-24 15:47:28 +0800

    upgraded datamodel

--- DataModel.php
+++ DataModel.php
@@ -9,64 +9,33 @@ function reverse_escape($str) {
 }
 
 function stripslashes_deep($value) {
-    return is_array($value) ? array_map('reverse_escape', $value) : reverse_escape($value);
+    return is_array($value)
+         ? array_map('reverse_escape', $value)
+         : reverse_escape($value);
 }
+
+function getMainDB() {
+    global $mysql_config, $maindb;
+    global $dbhost, $dbuser, $dbpasswd, $dbname;
+    if(!$maindb) {
+        $maindb = mysql_connect($dbhost, $dbuser, $dbpasswd);
+        mysql_select_db($dbname, $maindb);
+     // mysql_query("SET NAMES 'utf8mb4'"); @todo by @leaskh for emoji!!!
+        mysql_query("SET NAMES 'utf8'");
+    }
+    return $maindb;
+}
+
+$maindb = getMainDB();
 
 
 abstract class DataModel {
 
-    public function getHelperByNamev1($name) {
+    public function getHelperByName($name) {
         $class = ucfirst($name) . 'Helper';
-        $helperfile = HELPER_DIR. '/' . $class . '.php';
+        $helperfile = HELPER_DIR . '/' . $class . '.php';
         include_once $helperfile;
         return new $class;
-    }
-
-    public function getHelperByName($name, $version = '') {
-        if ($version == '') {
-            $this->getHelperByNamev1($name);
-        }
-        $class = ucfirst($name) . 'Helper';
-        $helperfile = HELPER_DIR . '/' . $version . '/' . $class . '.php';
-        include_once $helperfile;
-        return new $class;
-    }
-
-    private function endswith($str, $test) {
-        return substr($str, - strlen($test)) === $test;
-    }
-
-    private function needToConvertToFloat($name) {
-        return $this->endswith(strtolower($name), 'lat')
-            || $this->endswith(strtolower($name), 'lng');
-    }
-
-    private function needToConvertToInt($name) {
-        return $this->endswith(strtolower($name), 'id')
-            || $name == 'createdAt'
-            || $name == 'width'
-            || $name == 'height';
-    }
-
-    public function convertDataTypeForRow($row) {
-        foreach ($row as $key => $value) {
-            if ($this->needToConvertToInt($key)) {
-                $row[$key] = intval($value);
-            } else if ($this->needToConvertToFloat($key)) {
-                $row[$key] = floatval($value);
-            } else {
-                $row[$key] = $value;
-            }
-        }
-        return $row;
-    }
-
-    public function convertDataTypeForAll($all) {
-        $result = [];
-        foreach($all as $row) {
-            $result[] = $this->convertDataTypeForRow($row);
-        }
-        return $result;
     }
 
     public function mysql_fetch_all($result) {
@@ -77,7 +46,7 @@ abstract class DataModel {
         return $return;
     }
 
-    //check insert or delete is Error
+    // check insert or delete is Error
     public function query($sql) {
         mysql_query($sql);
         if (($error = mysql_error())) {
@@ -92,20 +61,17 @@ abstract class DataModel {
         }
     }
 
-    //get all
     public function getAll($sql) {
         if($query = mysql_query($sql)) {
             $return = $this->mysql_fetch_all($query);
             if(count($return)) {
                 return $return;
-                //return $this->convertDataTypeForAll($return);
             }
             //Return NULL Bug Fix
             return $return;
         }
     }
 
-    //get one row
     public function getRow($sql) {
         if($query = mysql_query($sql)) {
             if($return = mysql_fetch_array($query, MYSQL_ASSOC)) {
@@ -117,7 +83,7 @@ abstract class DataModel {
     public function countNum($sql) {
         $row = $this->getRow($sql);
         if(!empty($row)) {
-            foreach($row as $key=>$value) {
+            foreach($row as $key => $value) {
                 return $value;
             }
         } else {
@@ -139,21 +105,4 @@ abstract class DataModel {
         return $result;
     }
 
-    public function getData($sql){
-
-    }
-
 }
-
-function getMainDB() {
-    global $mysql_config, $maindb;
-    global $dbhost, $dbuser, $dbpasswd,$dbname;
-    if(!$maindb) {
-        $maindb = mysql_connect($dbhost, $dbuser, $dbpasswd);
-        mysql_select_db($dbname, $maindb);
-     // mysql_query("SET NAMES 'utf8mb4'"); @todo by @leaskh for emoji!!!
-        mysql_query("SET NAMES 'utf8'");
-    }
-    return $maindb;
-}
-$maindb = getMainDB();
