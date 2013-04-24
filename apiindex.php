456b16aca949a073a0360f1660a1b90b3be1892d
Leask Huang <i@leaskh.com>
2013-04-24 17:34:14 +0800

    tidy codes all over project and dele update_static_code_timestamp scrupt

--- apiindex.php
+++ apiindex.php
@@ -13,3 +13,9 @@ require_once 'common.php';
 require_once 'APIFrontController.php';
 
 FrontController::createInstance()->dispatch();
+
+
+            // "^/v1/([a-zA-Z]+)/(.*)$" => "apiindex.php?v=v1&class=$1&path=$2",
+            // "^/v2/([a-zA-Z]+)/([^?/]*)/([0-9]*)$" => "apiindex.php?v=v2&class=$1&path=$2&id=$3",
+            // "^/v2/([a-zA-Z]+)/([^?/]*)/([0-9]*)\?(.*)$" => "apiindex.php?v=v2&class=$1&path=$2&id=$3&$4",
+            // "^/v2/([a-zA-Z]+)/(.*)$" => "apiindex.php?v=v2&class=$1&path=$2",
