40153934d5258e67e525d2e974d5e6710f33845c
Leask Huang <i@leaskh.com>
2013-04-24 16:24:16 +0800

    working on new index.php and removed testemailjob.php

--- apiindex.php
+++ apiindex.php
@@ -3,20 +3,16 @@ error_reporting(E_ALL ^ E_NOTICE);
 session_start();
 #header('Content-Type: application/json; charset=UTF-8');
 
-define('PAGE_DIR', dirname(__FILE__) . '/pages');
-define('HELPER_DIR', dirname(__FILE__) . '/helpers');
-define('MODEL_DIR', dirname(__FILE__) . '/models');
+define('PAGE_DIR',          dirname(__FILE__) . '/pages');
+define('HELPER_DIR',        dirname(__FILE__) . '/helpers');
+define('MODEL_DIR',         dirname(__FILE__) . '/models');
 define('APICONTROLLER_DIR', dirname(__FILE__) . '/controllers/api');
-define('VIEW_DIR', dirname(__FILE__) . '/views');
-define('URL_PREFIX', '/exfe');
+define('VIEW_DIR',          dirname(__FILE__) . '/views');
+define('URL_PREFIX',        '/exfe');
 
 require_once 'common.php';
 require_once 'APIFrontController.php';
 
 define('REQUEST_METHOD', $_SERVER['REQUEST_METHOD']);
-
-define('INVITATION_MAYBE', 3);
-define('INVITATION_YES', 1);
-define('INVITATION_NO', 2);
 
 FrontController::createInstance()->dispatch();
