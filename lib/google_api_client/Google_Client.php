657c54db53b70134065a07d7dec03cd872592c5a
Leask Huang <i@leaskh.com>
2013-05-27 23:41:02 +0800

    working on google plus oauth, 5th

--- lib/google_api_client/Google_Client.php
+++ lib/google_api_client/Google_Client.php
@@ -37,8 +37,6 @@ if (! ini_get('date.timezone') && function_exists('date_default_timezone_set')) 
 set_include_path(dirname(__FILE__) . PATH_SEPARATOR . get_include_path());
 
 require_once "config.php";
-print_r($apiConfig);
-exit();
 // If a local configuration file is found, merge it's values with the default configuration
 if (file_exists(dirname(__FILE__)  . '/local_config.php')) {
   $defaultConfig = $apiConfig;
