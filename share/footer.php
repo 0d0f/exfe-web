e0b037ba093029da060a65436d06cd2387c84048
cfddream <cfddream@gmail.com>
2013-06-20 00:00:57 +0800

    add google_analytics.php

--- share/footer.php
+++ share/footer.php
@@ -28,20 +28,7 @@ if (JS_DEBUG) {
 }
 
 // Google Analytics
-if (SITE_URL === 'https://exfe.com') {
-echo <<<EOT
-<script>
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-31794223-2']);
-  _gaq.push(['_trackPageview']);
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-</script>
-EOT;
-}
+include 'google_analytics.php';
 
 if (SITE_URL !== 'https://exfe.com') {
     require 'jsdev.php';
