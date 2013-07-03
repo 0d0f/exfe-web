bdc038e6f60d8eaed659c98875fb58f503b6e675
cfddream <cfddream@gmail.com>
2013-07-03 17:27:24 +0800

    tweaks static files

--- share/footer.php
+++ share/footer.php
@@ -5,23 +5,14 @@ include 'ftconfig.php';
 echo "  </script>\n";
 echo "\n";
 
-$frontConfigFile = 'static/package.json';
-$frontConfigJson = file_get_contents($frontConfigFile);
-$frontConfigData = json_decode($frontConfigJson);
-
-if (!$frontConfigData) {
-    header('location: /500');
-    return;
-}
-
 if (JS_DEBUG) {
     foreach ($frontConfigData->desktop->dependencies as $script)  {
         addScript([[$script->name, $script->version]]);
     }
 } else {
     $filename = preg_replace(
-        '/{{version}}/',
-        $frontConfigData->desktop->version,
+        '/{{sha1}}/',
+        $frontConfigData->desktop->sha1,
         $frontConfigData->desktop->files->pro
     );
     rawAddScript($filename);
