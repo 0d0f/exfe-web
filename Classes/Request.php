6e07a8625fed84fdb4d5b2854b02f020b0841fb6
Leask Huang <i@leaskh.com>
2013-07-19 23:26:25 +0800

    finished request access apis

--- Classes/Request.php
+++ Classes/Request.php
@@ -2,45 +2,45 @@
 
 class Request extends Metainfo {
 
-    public $exfee_id     = null;
+    public $exfee_id    = null;
 
-    public $requested_by = null;
+    public $by_identity = null;
 
-    public $updated_by   = null;
+    public $updated_by  = null;
 
-    public $status       = null;
+    public $status      = null;
 
-    public $requested_at = null;
+    public $created_at  = null;
 
-    public $updated_at   = null;
+    public $updated_at  = null;
 
-    public $message      = null;
+    public $message     = null;
 
 
     public function __construct(
-        $id           = 0,
-        $exfee_id     = 0,
-        $requested_by = 0,
-        $updated_by   = 0,
-        $status       = 0,
-        $message      = '',
-        $requested_at = '',
-        $updated_at   = ''
+        $id          = 0,
+        $exfee_id    = 0,
+        $by_identity = null,
+        $updated_by  = null,
+        $status      = 0,
+        $message     = '',
+        $created_at  = '',
+        $updated_at  = ''
     ) {
-        parent::__construct((int) $id, 'requestaccess');
+        parent::__construct((int) $id, 'request');
 
-        $requested_at       = $requested_at ?: date('Y-m-d H:i:s');
-        $updated_at         = $updated_at   && $updated_at !== '0000-00-00 00:00:00'
-                            ? $updated_at    : $requested_at;
+        $created_at        = $created_at ?: date('Y-m-d H:i:s');
+        $updated_at        = $updated_at   && $updated_at !== '0000-00-00 00:00:00'
+                           ? $updated_at    : $created_at;
 
-        $arrStatus          = ['requesting', 'approved', 'declined', 'giveup'];
-        $this->exfee_id     = (int) $exfee_id;
-        $this->requested_by = $requested_by ?: null;
-        $this->updated_by   = $updated_by   ?: null;
-        $this->status       = $arrStatus[$status];
-        $this->requested_at = $requested_at  . ' +0000';
-        $this->updated_at   = $updated_at    . ' +0000';
-        $this->message      = $message      ?: '';
+        $arrStatus         = ['requesting', 'approved', 'declined', 'giveup'];
+        $this->exfee_id    = (int) $exfee_id;
+        $this->by_identity = $by_identity ?: null;
+        $this->updated_by  = $updated_by   ?: null;
+        $this->status      = $arrStatus[$status];
+        $this->created_at  = $created_at  . ' +0000';
+        $this->updated_at  = $updated_at    . ' +0000';
+        $this->message     = $message      ?: '';
     }
 
 }
