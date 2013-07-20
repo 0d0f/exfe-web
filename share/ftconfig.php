9f90295687a779ab8ab5beb53bc900eb44075278
cfddream <cfddream@gmail.com>
2013-07-20 11:55:48 +0800

    added routex template

--- share/ftconfig.php
+++ share/ftconfig.php
@@ -1,15 +1,16 @@
-// Front-End Configs
+// configs
 window._ENV_ = {
-  APP_MODE: '<?php echo JS_DEBUG ? 'development' : 'production'; ?>',
-  MAP_KEY: '<?php echo GOOGLE_MAP_KEY; ?>',
-  api_url: '<?php echo API_URL; ?>/v2',
-  img_url: '<?php echo IMG_URL; ?>',
-  site_url: '<?php echo SITE_URL; ?>',
-  streaming_api_url: '<?php echo STREAMING_API_URL; ?>',
-  app_scheme: '<?php echo APP_SCHEME; ?>',
-  timestamp: <?php echo Time(); ?>,
-  backgrounds: <?php echo json_encode($this->getVar('backgrounds')); ?>,
-  location: <?php echo json_encode($this->getVar('location')); ?>,
-  photo_providers: ['facebook', 'dropbox', 'flickr', 'instagram'],
-  timevalid: Math.abs(Math.round(+new Date() / 1000) - <?php echo Time(); ?>) < 15 * 60
+    APP_MODE: '<?php echo JS_DEBUG ? 'development' : 'production'; ?>'
+  , MAP_KEY: '<?php echo GOOGLE_MAP_KEY; ?>'
+  , api_url: '<?php echo API_URL; ?>/v2'
+  , img_url: '<?php echo IMG_URL; ?>'
+  , site_url: '<?php echo SITE_URL; ?>'
+  , streaming_api_url: '<?php echo STREAMING_API_URL; ?>'
+  , app_scheme: '<?php echo APP_SCHEME; ?>'
+  , timestamp: <?php echo Time(); ?>
+  , backgrounds: <?php echo json_encode($this->getVar('backgrounds')); ?>
+  , location: <?php echo json_encode($this->getVar('location')); ?>
+  , photo_providers: ['facebook', 'dropbox', 'flickr', 'instagram']
+  , timevalid: Math.abs(Math.round(+new Date() / 1000) - <?php echo Time(); ?>) < 15 * 60
+  , SMITH_ID: <?php echo SMITH_BOT_A; ?>,
 };
