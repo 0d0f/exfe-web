b0d74bbae2782fdea9e9c37621c90640abcc341e
Leask Huang <i@leaskh.com>
2013-07-31 17:20:06 +0800

    working on wechat

---
+++ api_controllers/v3/wechatActions.php
@@ -0,0 +1,17 @@
+<?php
+
+class wechatActions extends ActionController {
+
+    public function doIndex() {
+
+    }
+
+
+    public function doCallback() {
+
+    }
+
+
+    public function do
+
+}
