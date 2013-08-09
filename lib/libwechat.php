d36f9e99999699b2a87cc8800d7b2f0531764b85
Leask Huang <i@leaskh.com>
2013-08-09 17:10:27 +0800

    implement wechat routex map list

--- lib/libwechat.php
+++ lib/libwechat.php
@@ -40,27 +40,96 @@ class libwechat {
 
 
     public function packMessage(
-        $toUserName, $fromUserName, $content, $msgType = 'text'
+        $toUserName, $fromUserName, $content, $msgType = 'text', $FuncFlag = 0
     ) {
         if ($toUserName && $fromUserName && $content) {
-            $textTpl = "<xml>
-                        <ToUserName><![CDATA[%s]]></ToUserName>
-                        <FromUserName><![CDATA[%s]]></FromUserName>
-                        <CreateTime>%s</CreateTime>
-                        <MsgType><![CDATA[%s]]></MsgType>
-                        <Content><![CDATA[%s]]></Content>
-                        </xml>";
-            return sprintf(
-                $textTpl, $toUserName, $fromUserName, time(), $msgType,
-                self::xmlSafeStr($content)
-            );
+            $msg = [
+                'ToUserName'   => $toUserName,
+                'FromUserName' => $fromUserName,
+                'MsgType'      => $msgType,
+                'CreateTime'   => time(),
+                'FuncFlag'     => $FuncFlag,
+            ];
+            switch ($msgType) {
+                case 'text':
+                    $msg['Content']      = $content;
+                    break;
+                case 'music':
+                    // ['Title' => '', 'Description' => '', 'MusicUrl' => '', 'HQMusicUrl' => '']
+                    $msg['Music']        = $content;
+                    break;
+                case 'news':
+                    // [['Title' => '', 'Description' => '', 'PicUrl' => '', 'Url' => '']]
+                    $msg['ArticleCount'] = sizeof($content);
+                    $msg['Articles']     = $content;
+                    break;
+                default:
+                    return null;
+            }
+            return self::xml_encode($msg);
         }
         return null;
     }
 
 
     public static function xmlSafeStr($str) {
-        return preg_replace("/[\\x00-\\x08\\x0b-\\x0c\\x0e-\\x1f]/", '', $str);
+        return '<![CDATA[' . preg_replace(
+            "/[\\x00-\\x08\\x0b-\\x0c\\x0e-\\x1f]/", '', $str
+        ) . ']]>';
+    }
+
+
+    /**
+     * 数据XML编码
+     * @param mixed $data 数据
+     * @return string
+     */
+    public static function data_to_xml($data) {
+        $xml = '';
+        foreach ($data as $key => $val) {
+            is_numeric($key) && $key = "item id=\"{$key}\"";
+            $xml .= "<{$key}>";
+            $xml .= (is_array($val) || is_object($val))
+                  ? self::data_to_xml($val)
+                  : self::xmlSafeStr($val);
+            list($key,) = explode(' ', $key);
+            $xml .=  "</{$key}>";
+        }
+        return $xml;
+    }
+
+
+    /**
+     * XML编码
+     * @param mixed $data 数据
+     * @param string $root 根节点名
+     * @param string $item 数字索引的子节点名
+     * @param string $attr 根节点属性
+     * @param string $id   数字索引子节点key转换的属性名
+     * @param string $encoding 数据编码
+     * @return string
+    */
+    public function xml_encode(
+        $data,
+        $root     = 'xml',
+        $item     = 'item',
+        $attr     = '',
+        $id       = 'id',
+        $encoding = 'utf-8'
+    ) {
+        if (is_array($attr)) {
+            $_attr = [];
+            foreach ($attr as $key => $value) {
+                $_attr[] = "{$key}=\"{$value}\"";
+            }
+            $attr = implode(' ', $_attr);
+        }
+        $attr = trim($attr);
+        $attr = empty($attr) ? '' : " {$attr}";
+        $xml  = "<{$root}{$attr}>";
+        $xml .= self::data_to_xml($data, $item, $id);
+        $xml .= "</{$root}>";
+        return $xml;
     }
 
 
