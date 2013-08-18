c62eb5f12bae777ab25b15ba23d27e82b1673126
cfddream <cfddream@gmail.com>
2013-08-18 13:59:41 +0800

    update mobile header
    (cherry picked from commit 9cf594ab5c983b9bb5bba59ce81d5f6b2cebf775)

--- share/header_mobile.php
+++ share/header_mobile.php
@@ -21,3 +21,4 @@
   <meta name="authorization" content="<?php echo htmlentities(json_encode($oauth)); ?>" />
 <?php } ?>
   <link rel="icon" type="image/x-icon" href="/static/img/favicon.ico" />
+  <link rel="stylesheet" media="screen" type="text/css" href="/static/css/<?php echo $frontConfigData->css->exfemobilemin; ?>" />
