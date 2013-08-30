03e84bcbe0712d5e8487a6f5c43682ec656a679b
Leask Huang <i@leaskh.com>
2013-08-30 18:53:50 +0800

    upgraded HistoryModels

--- models/HistoryModels.php
+++ models/HistoryModels.php
@@ -1,33 +1,50 @@
 <?php
 
+// CREATE TABLE `history` (
+//     `id`         bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+//     `updated_by` bigint(20) unsigned NOT NULL,
+//     `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+//     `module`     varchar(255) NOT NULL,
+//     `action`     varchar(255) NOT NULL,
+//     `module_id`  varchar(255),
+//     `data`       text,
+//     `index_id`   bigint(20) unsigned NOT NULL,
+//     PRIMARY KEY (`id`)
+// ) ENGINE = MyISAM DEFAULT CHARSET = utf8mb4;
+
 class HistoryModels extends DataModel {
 
-    public function addLog($from_obj, $from_id, $action, $to_obj, $to_id, $to_field, $change_summy, $meta = '') {
-        $from_obj=dbescape($from_obj);
-        $from_id=intval($from_id);
-        $action=dbescape($action);
-        $to_obj=dbescape($to_obj);
-        $to_id=intval($to_id);
-        $to_field=dbescape($to_field);
-        $change_summy=dbescape($change_summy);
-        $meta=dbescape($meta);
-
-        $sql="insert into logs (from_obj, from_id, action, to_obj, to_id, to_field, change_summy, meta, time) values ('$from_obj',$from_id,'$action','$to_obj',$to_id,'$to_field','$change_summy','$meta', NOW());";
-        $this->query($sql);
+    public function log($by_identity_id, $module, $module_id, $action, $data, $index_id = '') {
+        $by_identity_id = (int) $by_identity_id;
+        $module         = dbescape($module);
+        $module_id      = dbescape($module_id);
+        $action         = dbescape($action);
+        $data           = dbescape(json_encode($data));
+        $index_id       = dbescape($index_id);
+        return $this->query(
+            "INSERT INTO `history` SET
+             `updated_by` =  {$by_identity_id},
+             `module`     = '{$module}',
+             `module_id`  = '{$module_id}',
+             `action`     = '{$action}',
+             `data`       = '{$data}',
+             `index_id`   =  {$index_id};"
+        );
     }
 
-    public function getRecentlyLogsByCrossIds($cross_ids, $time = '', $limit = 1000) {
-        if ($cross_ids) {
-            $cross_ids = implode(' OR `to_id` = ', $cross_ids);
-            if ($time) {
-                $sql = "SELECT * FROM `logs` WHERE `to_obj` = 'cross' AND (`to_id` = {$cross_ids}) AND time > '{$time}' ORDER BY `id` DESC LIMIT {$limit};";
-            } else {
-                $sql = "SELECT * FROM `logs` WHERE `to_obj` = 'cross' AND (`to_id` = {$cross_ids}) ORDER BY `id` DESC LIMIT {$limit};";
-            }
-            return $this->getAll($sql);
-        } else {
-            return array();
+
+    public function getLogs($index_id, $module = '', $module_id = '', $limit = 1000) {
+        $arrWhere = ["`index_id` = '" . dbescape($index_id) . "'"];
+        if ($module) {
+            $arrWhere[] = "`module`    = '" . dbescape($module)    . "'";
         }
+        if ($module_id) {
+            $arrWhere[] = "`module_id` = '" . dbescape($module_id) . "'";
+        }
+        $strWhere = implode(' AND ', $arrWhere);
+        return $this->getAll(
+            "SELECT * FROM `history` WHERE {$strWhere} ORDER BY `id` DESC LIMIT {$limit};"
+        );
     }
 
 }
