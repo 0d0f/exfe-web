f2b8a51bae66ea9a7dc3c263cb7bfd75af0f894a
Leask Huang <i@leaskh.com>
2013-09-03 20:31:17 +0800

    update wechat apis

--- models/WechatModels.php
+++ models/WechatModels.php
@@ -74,7 +74,7 @@ class WechatModels extends DataModel {
 
 
     public function requestXTitle($cross_id, $cross_title, $external_id) {
-        setCache("wechat_user_{$external_id}_current_x_id", $cross_id, 60);
+        setCache("wechat_user_{$external_id}_current_x_id", $cross_id, 60 * 2);
         return $this->sendTemplateMessage(
             $external_id, 'x_title_update', ['cross' => ['title' => $cross_title]]
         );
@@ -91,12 +91,12 @@ class WechatModels extends DataModel {
             'gh_c2e5730627a4' => [
                 'x_title_update'        => 'cKExQY5C6M20Sk6dNzajAqDPkryvIgGz6nWpnlbQlj5JQjshzG_gQ0F18RsJeWH1',
                 'user_location_request' => '0w_9XXPiMHmqKWgrQB-zzkujNgDgG1JRGN8j132SiNEx0HcqXu8a1G_xTLwedrmW',
-                'x_join'                => '',
+                'x_join'                => 'Nb5n4d_tNHcRgWjz9gBk1Y5A3nM-roYYJdbKe5mh0BLc5k-grCSi08_n5qh4Irnj',
             ],
             'gh_8c4c8d9d14a7' => [
                 'x_title_update'        => 'cEhPMpIuw87cGZKvZpWjCru_I7LW-SerUzLHlzYyy2px1ao16opH6_Qld8H96Lec',
                 'user_location_request' => 'F2b3C5kpw2lDPyYlUAygr6X1STqdHclR9vKMhBxsEHXO7IgwJ-oI8gBUdhfutePU',
-                'x_join'                => '',
+                'x_join'                => 'WOF8SzXspPuLjrBMD-mYbwP0Z-xNYwPgAl5Tgy5Mc7uulKulrggUU_R-W4fRZF9A',
             ],
         ];
         $ids = splitIdentityId($toUserName);
