39b207521966352c84de25647108698f640d8ffe
cfddream <cfddream@gmail.com>
2013-09-05 18:04:56 +0800

    updates mobile view

--- views/home/mobileView.php
+++ views/home/mobileView.php
@@ -313,6 +313,25 @@
           <div class="loading"><div class="spinner"><div class="mask"><div class="maskedCircle"></div></div></div></div>
         </div>
       </div>
+      <div id="privacy-dialog" class="dialog hide">
+        <div class="main">
+          <h3>隐私至关重要</h3>
+          <p class="p0">您刚刚拒绝开启这张“活点地图”：Threshold of the odyssey。它将不会展现您的位置，您也无法用它看到别人的位置。但这不会影响您已开启的其它“活点地图”页面，每张地图中是否展现您的位置是各自独立的设置。 </p>
+          <p class="p1">像“活点地图”这样能获知您方位的工具，应以最高标准尊重个人隐私和数据安全。我们对此非常重视。</p>
+          <div class="btn">
+            <div class="tip">要改变主意与朋友们互相看到位置和轨迹？</div>
+            <button id="turn-on">开启这张活点地图</button>
+          </div>
+        </div>
+      </div>
+      <div id="wechat-guide-dialog" class="dialog hide">
+        <div class="main">
+          <div class="ibox">
+            <p><img src="/static/img/wechatbtn_accverified@2x.png" alt="" width="40" height="40" align="right" />请从这里找到右侧 图标“查看公众号”。</p>
+            <p><span>关注服务号</span>以便正常使用，也方便您及时收到朋友们的邀请和提醒通知。</p>
+          </div>
+        </div>
+      </div>
     </div>
   </script>
 
@@ -337,25 +356,6 @@
             </div>
           </div>
           <div id="cleanup-cache">重启活点地图</div>
-        </div>
-      </div>
-      <div id="privacy-dialog" class="dialog hide">
-        <div class="main">
-          <h3>隐私至关重要</h3>
-          <p class="p0">您刚刚拒绝开启这张“活点地图”：Threshold of the odyssey。它将不会展现您的位置，您也无法用它看到别人的位置。但这不会影响您已开启的其它“活点地图”页面，每张地图中是否展现您的位置是各自独立的设置。 </p>
-          <p class="p1">像“活点地图”这样能获知您方位的工具，应以最高标准尊重个人隐私和数据安全。我们对此非常重视。</p>
-          <div class="btn">
-            <div class="tip">要改变主意与朋友们互相看到位置和轨迹？</div>
-            <button id="turn-on">开启这张活点地图</button>
-          </div>
-        </div>
-      </div>
-      <div id="wechat-guide-dialog" class="dialog hide">
-        <div class="main">
-          <div class="ibox">
-            <p><img src="/static/img/wechatbtn_accverified@2x.png" alt="" width="40" height="40" align="right" />请从这里找到右侧 图标“查看公众号”。</p>
-            <p><span>关注服务号</span>以便正常使用，也方便您及时收到朋友们的邀请和提醒通知。</p>
-          </div>
         </div>
       </div>
   </script>
