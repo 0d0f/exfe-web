88a5b9088f9f50ab656b029facf3b09e8bf2dfa8
cfddream <cfddream@gmail.com>
2013-09-06 11:43:15 +0800

    updates mobile view

--- views/home/mobileView.php
+++ views/home/mobileView.php
@@ -328,7 +328,7 @@
         <div class="main">
           <div class="ibox">
             <p><img src="/static/img/wechatbtn_accverified@2x.png" alt="" width="40" height="40" align="right" />请从这里找到右侧 图标“查看公众号”。</p>
-            <p><span>关注服务号</span>以便正常使用，也方便您及时收到朋友们的邀请和提醒通知。</p>
+            <p><span>关注</span>服务号以便正常使用，也方便您及时收到朋友们的邀请和提醒通知。</p>
           </div>
         </div>
       </div>
