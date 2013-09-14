aced95b3e5da0e6feeb45d16bfdedd125d8be600
cfddream <cfddream@gmail.com>
2013-09-14 15:13:45 +0800

    adds hide className for static maps

--- views/home/mobileView.php
+++ views/home/mobileView.php
@@ -281,7 +281,7 @@
   <script id="routex-tmpl" type="text/x-handlebars-template">
     <div id="routex">
       <div id="map"></div>
-      <div id="static-map"></div>
+      <div id="static-map" class="hide"></div>
       <!--svg xmlns="http://www.w3.org/2000/svg" version="1.1" id="svg"></svg-->
       <canvas id="canvas"></canvas>
       <div id="identities-overlay">
