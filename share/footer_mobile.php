2975153c6f3cbe74c41754a66fffdb215fdfe125
cfddream <cfddream@gmail.com>
2013-09-18 19:12:49 +0800

    Update mobile view

--- share/footer_mobile.php
+++ share/footer_mobile.php
@@ -8,19 +8,14 @@ $jsname = preg_replace(
     JS_DEBUG ? $frontConfigData->mobile->files->dev : $frontConfigData->mobile->files->pro
 );
 
-$cssname = $frontConfigData->css->exfemobilemin;
-
 if ($_GET['debug']) {
   $file_randtime = strtotime("now");
   echo "<script src='/static/js/debugger/0.0.1/debugger.js?" . $frontConfigData->mobile->standalone->debugger->sha1 . $file_randtime . "'></script>";
-  $cssname .= '?' . $file_randtime;
   $jsname .= '?' . $file_randtime;
 }
 
 echo "  <script>\n";
 include 'ftconfig.php';
-echo "window._ENV_.JSFILE = '${jsname}';\n";
-echo "window._ENV_.CSSFILE = '${cssname}';\n";
 echo 'window._ENV_.smith_id = ' . $this->getVar('smith_id') . ';' . "\n";
 echo 'window._ENV_.exfee_id = ' . $this->getVar('exfee_id') . ';' . "\n";
 echo 'window._ENV_.cross = ' . $this->getVar('cross') . ';' . "\n";
